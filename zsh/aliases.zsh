ef7980cfe93dbd786e79f78e5c5c1a8d48142e74
Kuntau <kuntau17@gmail.com>
2013-11-07 07:45:16 -0500

    remove format in youtube-dl for audio extract alias modified ls to display directories first commented out OS specific ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,8 +16,8 @@ alias 'yt3=youtube-dl -f 43'
 alias 'yt4=youtube-dl -f 44'
 alias 'yt7=youtube-dl -f 45'
 alias 'yt1=youtube-dl -f 46'
-alias 'yta=youtube-dl -f 45 --extract-audio'
-alias 'ytav=youtube-dl -f 45 --extract-audio --keep-video'
+alias 'yta=youtube-dl --extract-audio'
+alias 'ytav=youtube-dl --extract-audio --keep-video'
 
 # mounted volumes shortcut
 multi='/mnt/multi'
@@ -94,21 +94,21 @@ else # OS X `ls`
 fi
 
 # List all files colorized in long format
-# alias l="ls -l ${colorflag}"
+alias l="ls --group-directories-first ${colorflag}"
 
 # List all files colorized in long format, including dot files
-alias la="ls -la ${colorflag}"
+alias la="ls -laG --group-directories-first --si ${colorflag}"
 
 # List only directories
 alias lsd='ls -l | grep "^d"'
 
 # Always use color output for `ls`
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-  alias ls="command ls -G"
-else
-  alias ls="command ls --color"
-  export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-fi
+# if [[ "$OSTYPE" =~ ^darwin ]]; then
+#   alias ls="command ls -G"
+# else
+#   alias ls="command ls --color"
+#   export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+# fi
 
 # `cat` with beautiful colors. requires Pygments installed.
 #                  sudo easy_install Pygments
