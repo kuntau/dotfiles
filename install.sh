85c5795d05ae36b46c349e893d3123de24a4bc07
Kuntau <kuntau17@gmail.com>
2014-08-06 13:20:51 +0800

    now really fix it -_-

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # install dotfiles script
 
 # an array of files to symlinks
-dotfiles=( .vimrc .gitignore .gitconfig .zshrc .ideavimrc )
+dotfiles=( vimrc gitignore gitconfig zshrc ideavimrc )
 # dotfiles=( 'vimrc' 'gitignore' 'gitconfig' 'zshrc' 'ideavimrc' )
 
 echo "Checking requirements..."
@@ -17,9 +17,10 @@ do
   echo "Installing $i config files"
   if [ -f ~/$i ] || [ -h ~/$i ]; then
     echo "File exists.. The file $i will be backup to $i.bak"
-    ln -s ~/dotfiles/$1 ~/.$1
-    echo ""
+    mv $i $i.bak
   fi
+  ln -s ~/dotfiles/$i ~/.$i
+  echo ""
 done
 
 # configure oh-my-zsh
@@ -39,7 +40,7 @@ ln -s ~/dotfiles/zsh/kuntau.zsh-theme ~/.oh-my-zsh/themes/kuntau.zsh-theme
 
 # append path.. possible duplicate..
 # echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
-export PATH=\$PATH:$PATH >> ~/.zshrc
+# export PATH=\$PATH:$PATH >> ~/.zshrc
 
 # echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
 # sudo chsh -s `which zsh`
