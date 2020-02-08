4010fc1ce67e74242a0d25c8ebeda3491de09efa
Nizamuddin Sulieman <kuntau17@gmail.com>
2020-02-08 08:59:07 +0800

    add support for Exa if available

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,31 @@
 # alias 'dus=du -sckx * | sort -nr'
 alias dus='du -hd 1'
-alias l='ls -l'
+
+# files listing with optional exa -- colorized  everything
+if command -v exa &> /dev/null; then
+  alias l='exa'
+  alias ll='exa --long --group-directories-first'
+  alias la='exa -alg --group-directories-first'
+  alias laa='exa -alg -a --group-directories-first'
+  alias lsd='exa --long --only-dirs' # List only directories
+  alias lst='exa --long --tree --level=2 --group-directories-first' # Tree view
+  alias lsr='exa --long --recurse --level=2 --group-directories-first' # Recurse directories
+else
+  alias l="ls ${colorflag}" # List all files colorized in long format
+  alias la="ls -laGh ${colorflag}" # List all files colorized in long format, including dot files
+  alias lsd='ls -l | grep "^d"' # List only directories
+fi
+
+
+
+# Always use color output for `ls`
+# if [[ "$OSTYPE" =~ ^darwin ]]; then
+#   alias ls="command ls -G"
+# else
+#   alias ls="command ls --color"
+#   export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+# fi
+
 
 # config file shortcut
 alias zshrc='$EDITOR ~/.zshrc'
@@ -108,23 +133,6 @@ if ls --color > /dev/null 2>&1; then # GNU `ls`
 else # OS X `ls`
   colorflag="-G"
 fi
-
-# List all files colorized in long format
-alias l="ls ${colorflag}"
-
-# List all files colorized in long format, including dot files
-alias la="ls -laGh ${colorflag}"
-
-# List only directories
-alias lsd='ls -l | grep "^d"'
-
-# Always use color output for `ls`
-# if [[ "$OSTYPE" =~ ^darwin ]]; then
-#   alias ls="command ls -G"
-# else
-#   alias ls="command ls --color"
-#   export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-# fi
 
 # `cat` with beautiful colors. requires Pygments installed.
 #                  sudo easy_install Pygments
