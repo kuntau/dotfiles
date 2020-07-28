15e3a781e01089b6522e9e91ae7699d34c37366c
Nizamuddin Sulieman <kuntau17@gmail.com>
2020-07-29 04:00:06 +0800

    Add `VISUAL` env for linux

--- zsh/default.zsh
+++ zsh/default.zsh
@@ -3,3 +3,6 @@ hash bat 2> /dev/null && export PAGER='bat -p'
 
 export EDITOR='vim'
 hash nvim 2> /dev/null && export EDITOR='nvim'
+
+export VISUAL='vim'
+hash nvim 2> /dev/null && export VISUAL='nvim'
