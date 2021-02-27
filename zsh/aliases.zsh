f8737b555768e37563fa510d42b41481e601105b
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-02-27 10:29:43 +0800

    Add `wget` alias for website mirror with throttle

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -104,6 +104,20 @@ alias wgetm="wget \
   --no-clobber \
   $1"
 
+# wget throttle
+alias wgett="wget \
+  --header='Accept: text/html' \
+  --user-agent='Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/84.0.4147.140 Safari/537.36' \
+  --mirror \
+  --convert-links \
+  --adjust-extension \
+  --page-requisites \
+  --no-parent \
+  --wait=1 \
+  --limit-rate=200K \
+  -q --show-progress --continue \
+  $1"
+
 # recursive wget download
 # --cut-dirs=1 \
 alias wgetr='wget \
