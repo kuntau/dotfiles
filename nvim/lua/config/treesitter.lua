cc05dbc09eb9224d59f45a4a9a6bbff30aeeb881
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-11 03:52:38 +0800

    Add treesitter `ensure_installed` list Add treesitter `incremental_selection` module Add treesitter `swap` module Add treesitter `lsp_interop` module

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,10 +1,26 @@
 -- treesitter.lua
 
+-- TODO: investigate how to set this in lua
+-- RESULT: Yep, we're overthinking it.. just set as literal string LOL
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
 require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'bash', 'cmake', 'comment', 'cpp', 'css', 'dart', 'dockerfile', 'go', 'graphql', 'html', 'javascript', 'json', 'jsonc', 'lua', 'php', 'python', 'regex', 'ruby', 'scss', 'svelte', 'toml', 'tsx', 'typescript', 'vim', 'vue', 'yaml' },
+  sync_install = true,
   highlight = {
     enable = true,
     use_languagetree= true,
-    addtional_vim_regex_highlighting = false,
+    addtional_vim_regex_highlighting = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm'
+    }
   },
   indent = {
     enable = true,
@@ -56,6 +72,24 @@ require('nvim-treesitter.configs').setup({
         },
       },
     },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+    lsp_interop = {
+      enable = true,
+      border = 'none',
+      peek_definition_code = {
+        ["<leader>df"] = "@function.outer",
+        ["<leader>dF"] = "@class.outer",
+      },
+    },
+  },
   context_commentstring = {
     enable = true
   },
