de068ba1af676433041738bb97e57a86e2b56103
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-12 05:36:46 +0800

    Feature: Add treesitter `move` configs

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -72,6 +72,26 @@ require('nvim-treesitter.configs').setup({
         },
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
     swap = {
       enable = true,
       swap_next = {
