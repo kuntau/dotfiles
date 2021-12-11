11645deebc2029fc051ab0010ad8203cb8bf9218
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-12 04:54:04 +0800

    Change `map` to local function

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -10,18 +10,7 @@ M.feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
--- Help in new tabs
-M.quickClosePane = function()
-  if vim.o.buftype == 'help' then -- if it's help pane, do some modifications
-    vim.cmd('resize') -- resize help buffer to maximum (CTRL-W__)
-    -- vim.cmd('wincmd T') -- move current buffer to new tab
-  end
-  M.nmap('q', ':q<cr>', { buffer = true }) -- map `q` to close help buffer
-end
-
-vim.cmd 'autocmd init_lua FileType help,qf,spectre_panel lua require("utils").quickClosePane()' -- 1st choice
-
-M.map = function (mode, lhs, rhs, opts)
+local map = function (mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts and type(opts) == 'table' then
     for key, value in pairs(opts) do
@@ -40,23 +29,25 @@ M.map = function (mode, lhs, rhs, opts)
 end
 
 M.nmap = function (lhs, rhs, opts)
-  M.map('n', lhs, rhs, opts)
+  map('n', lhs, rhs, opts)
 end
 
 M.vmap = function (lhs, rhs, opts)
-  M.map('v', lhs, rhs, opts)
+  map('v', lhs, rhs, opts)
 end
 
 M.imap = function (lhs, rhs, opts)
-  M.map('i', lhs, rhs, opts)
+  map('i', lhs, rhs, opts)
 end
 
 M.xmap = function (lhs, rhs, opts)
-  M.map('x', lhs, rhs, opts)
+  map('x', lhs, rhs, opts)
 end
 
 M.omap = function (lhs, rhs, opts)
-  M.map('x', lhs, rhs, opts)
+  map('x', lhs, rhs, opts)
+end
+
 M.reloadModule = function ()
   local module = vim.fn.expand('%:t:r')
   require('plenary.reload').reload_module(module)
