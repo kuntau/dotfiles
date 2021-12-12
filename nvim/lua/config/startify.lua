d1b267127bab6220790730afb2925f296f254905
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-12 12:48:12 +0800

    Feat: Add `gitCommit` to list (default: 5) last commit message. Each commit will have associated _cmd_ for `git show <commit hash>` Refactor: Make _git util functions_ in `utils` generic Refactor: Create _startify_ specific helper/formatter in `startify.lua` chore: Prettier _CWD_

--- nvim/lua/config/startify.lua
+++ nvim/lua/config/startify.lua
@@ -1,13 +1,45 @@
 -- vim-startify configs
 
+local utils = require('utils')
+
+local formatter = function (files)
+  local results = {}
+  for _, file in ipairs(files) do
+    table.insert(results, { line = file, path = file })
+  end
+  return results
+end
+
+local gitModified = function ()
+  return formatter(utils.gitModified())
+end
+
+local gitUntracked = function ()
+  return formatter(utils.gitUntracked())
+end
+
+local gitCommit = function ()
+  -- local files = formatter(utils.gitListCommit())
+  local results = {}
+  for _, file in ipairs(utils.gitListCommit()) do
+    table.insert(results, { line = vim.fn.matchstr(file, '\\s\\zs.*'), cmd = '!git show ' .. vim.fn.matchstr(file, '^\\x\\+') })
+  end
+  return results
+end
+
+local cwd = function ()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+end
+
 vim.g.startify_lists = {
-  { header =  {('   MRU ' .. vim.fn.getcwd())}, type = 'dir' },
-  { header =  {'   Global MRU'},                type = 'files' },
-  { header =  {'   Sessions'},                  type = 'sessions' },
-  { header =  {'   Bookmarks'},                 type = 'bookmarks' },
-  { header =  {'   Git modified'},              type = require('utils').gitModified },
-  { header =  {'   Git untracked'},             type = require('utils').gitUntracked },
-  { header =  {'   Commands'},                  type = 'commands' },
+  { header = {('   MRU: ' .. cwd())}, type = 'dir' },
+  { header = {'   MRU: Global'},      type = 'files' },
+  { header = {'   Sessions'},         type = 'sessions' },
+  { header = {'   Git modified'},     type = gitModified },
+  { header = {'   Git untracked'},    type = gitUntracked },
+  { header = {'   Git commits'},      type = gitCommit },
+  { header = {'   Bookmarks'},        type = 'bookmarks' },
+  { header = {'   Commands'},         type = 'commands' },
 }
 
 vim.g.startify_bookmarks = {
@@ -15,12 +47,13 @@ vim.g.startify_bookmarks = {
 }
 
 vim.g.startify_commands = {
-  { p = { 'Upgrade Plug', 'PlugUpgrade' }},
-  { u = { 'Update Plugins', 'PlugUpdate' }},
-  { tsu = { 'Treesitter Update', 'TSUpdate' }},
-  { tss = { 'Treesitter UpdateSync', 'TSUpdateSync' }},
+  { pi = { 'Plug Install', 'PlugInstall' }},
+  { pg = { 'Upgrade Plug', 'PlugUpgrade' }},
+  { pu = { 'Update Plugins', 'PlugUpdate' }},
+  { tu = { 'Treesitter Update', 'TSUpdate' }},
+  { ts = { 'Treesitter UpdateSync', 'TSUpdateSync' }},
   { ch = { 'Check Health', 'checkhealth' }},
-  { lsp = { 'LSP Info', 'LspInfo' }},
+  { ls = { 'LSP Info', 'LspInfo' }},
 }
 
 vim.g.startify_disable_at_vimenter = 0
@@ -38,15 +71,14 @@ vim.g.startify_fortune_use_unicode = 1
 vim.g.startify_custom_header = 0
 -- vim.g.startify_custom_header = 'startify#center(startify#fortune#boxed())'
 
-vim.g.startifyx_custom_header = [[
-                                         ▟▙            
-                                         ▝▘            
- ██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄   ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖
- ██▛▔ ▝██  ██▄▄▄▄██  ██▛▔▔▜██  ▝██  ██▘  ██  ██▛▜██▛▜██
- ██    ██  ██▀▀▀▀▀▘  ██▖  ▗██   ▜█▙▟█▛   ██  ██  ██  ██
- ██    ██  ▜█▙▄▄▄▟▊  ▀██▙▟██▀   ▝████▘   ██  ██  ██  ██
- ▀▀    ▀▀   ▝▀▀▀▀▀     ▀▀▀▀       ▀▀     ▀▀  ▀▀  ▀▀  ▀▀
-
-]]
+vim.g.startify_custom_footer = {
+'                                            ▟▙            ',
+'                                            ▝▘            ',
+'    ██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄   ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖',
+'    ██▛▔ ▝██  ██▄▄▄▄██  ██▛▔▔▜██  ▝██  ██▘  ██  ██▛▜██▛▜██',
+'    ██    ██  ██▀▀▀▀▀▘  ██▖  ▗██   ▜█▙▟█▛   ██  ██  ██  ██',
+'    ██    ██  ▜█▙▄▄▄▟▊  ▀██▙▟██▀   ▝████▘   ██  ██  ██  ██',
+'    ▀▀    ▀▀   ▝▀▀▀▀▀     ▀▀▀▀       ▀▀     ▀▀  ▀▀  ▀▀  ▀▀',
+}
 
 -- vim.g.startify_custom_header = startify_ascii_header
