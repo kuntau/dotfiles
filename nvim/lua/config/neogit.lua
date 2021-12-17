bedce6e56e68dad00bf7e85a98c23db934d9c216
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-18 00:44:15 +0800

    chore(config): Change `neogit` configs. Add `Diffview` integration.

--- nvim/lua/config/neogit.lua
+++ nvim/lua/config/neogit.lua
@@ -1,8 +1,22 @@
 -- neogit configs
 
 require('neogit').setup({
+  integrations = {
+    diffview = true
+  },
   commit_popup = {
     kind = "split"
   },
-  kind = "split_above",
+  kind = "vsplit",
+  signs = {
+    -- { closed, open }
+    section = { "❱", "▼" },
+    item = { "", "" },
+    hunk = { "›", "ˬ" },
+  },
+  disable_signs = false,
+  disable_hint = true,
+  disable_commit_confirmation = true,
+  disable_builtin_notifications = false,
+  disable_insert_on_commit = false,
 })
