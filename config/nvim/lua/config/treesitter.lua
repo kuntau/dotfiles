168034e0fdb5d0c60d0d5f13d3d9172a2893fb96
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-23 03:48:55 +0800

    refactor(treesitter): Swap `function` jump shortcuts with `class` * So far we use more function jump than class, so it's more convenient.

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -76,20 +76,20 @@ require('nvim-treesitter.configs').setup({
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
+        ["]]"] = "@function.outer",
+        ["]m"] = "@class.outer",
       },
       goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
+        ["]["] = "@function.outer",
+        ["]M"] = "@class.outer",
       },
       goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
+        ["[["] = "@function.outer",
+        ["[m"] = "@class.outer",
       },
       goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
+        ["[]"] = "@function.outer",
+        ["[M"] = "@class.outer",
       },
     },
     swap = {
