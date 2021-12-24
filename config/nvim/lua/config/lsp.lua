5f1460ebeb7b83cd89a7ce7bcc05656c0446a6ad
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-24 18:34:50 +0800

    fix(nvim.lsp): `vim.lsp.buf.implementation` should use _insert mode_

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -34,11 +34,12 @@ end
 local nvim_lsp = require('lspconfig')
 local configs = require('lspconfig.configs')
 local nmap = require('utils').nmap
+local imap = require('utils').imap
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  -- local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
@@ -48,11 +49,11 @@ local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  nmap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  nmap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  nmap('gd', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  nmap('<c-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   nmap('K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  nmap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  nmap('<Leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  nmap('<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  imap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   nmap('<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   nmap('<Leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   nmap('<Leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -98,7 +99,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'sumneko_lua', 'tsserver', 'intelephense', 'html', 'jsonls', 'cssls', 'eslint', 'ls_emmet', 'volar' }
+local servers = { 'sumneko_lua', 'tsserver', 'intelephense', 'html', 'jsonls', 'cssls', 'eslint', 'ls_emmet', 'volar', 'vimls' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
