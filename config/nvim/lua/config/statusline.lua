3203a8bd1f1c31537df18dbeb08314a05127948e
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-24 18:38:30 +0800

    chore(nvim.line): Add more _filetype_, formatting

--- config/nvim/lua/config/statusline.lua
+++ config/nvim/lua/config/statusline.lua
@@ -1,36 +1,29 @@
 -- neovim status line configs
 -- could be any of powerline, windline, lualine, airline or lightline
 
--- require('wlsample.airline')
--- require('feline').setup()
-
 local ft_extension = {
     sections = {lualine_a = {'filetype'}},
     filetypes = {'startify', 'Trouble', 'undotree', 'vim-plug', 'DiffviewFiles', 'startuptime'}
 }
 
 local neogit_extension = {
-    sections = {lualine_a = {'branch'}},
-    filetypes = {'NeogitStatus'}
+  sections = { lualine_a = {'filetype'}, lualine_b = {'branch'} },
+  filetypes = {'NeogitStatus', 'NeogitCommitMessage', 'NeogitPopup', 'NeogitLogView', 'NeogitGitCommandHistory'}
 }
 
 require('lualine').setup({
   options = {
-    component_separators = '',
-    disabled_filetypes = {}
-    -- section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '·'},
+    disabled_filetypes = {},
   },
   tabline = {
-    lualine_a = {},
-    lualine_b = {},
     lualine_c = { require'tabline'.tabline_buffers },
-    lualine_x = { require'tabline'.tabline_tabs },
-    lualine_y = {},
-    lualine_z = {},
+    lualine_x = { require'tabline'.tabline_tabs  },
   },
   extensions = {
     'nvim-tree',
     'quickfix',
+    'symbols-outline',
     ft_extension,
     neogit_extension,
   }
