99751e9916a2f4f416d09fea6780d87d8cea394f
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-26 06:01:43 +0800

    refactor(nvim): Playing with `packer.nvim` lazy loading

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,18 +1,20 @@
 -- Plugins with packer.nvim
 
 local fn = vim.fn
-local packer_path = '/site/pack/packer/start/packer.nvim'
 local packer_repo = 'https://github.com/wbthomason/packer.nvim'
-local install_path = fn.stdpath('data') .. packer_path
+local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 local packer_bookstrap
 
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bookstrap = fn.system({'git', 'clone', '--depth', '1',packer_repo, install_path})
+  -- print('BOOTSTRAP clone: '..packer_bootstrap)
 end
+
+vim.cmd 'packadd packer.nvim'
 
 local status_ok, packer = pcall(require, 'packer')
 if not status_ok then
-  error 'Something wrong while requiring packer'
+  print('Something wrong while requiring packer')
   return
 end
 
@@ -24,94 +26,110 @@ return packer.startup({function(use)
   end
 
   -- Essentials
-  use 'wbthomason/packer.nvim' -- Packer can manage itself
   use 'nvim-lua/plenary.nvim'
   use 'kuntau/vim-osc52' -- Copy & paste across tmux & screen over mosh
-  use{'mhinz/vim-startify', config = function () require('config.startify') end}
-  use{'nvim-telescope/telescope.nvim', config = function () require('config.telescope') end}
-  use{'kyazdani42/nvim-tree.lua', config = function () require('config.nvimtree') end}
-  use{'mbbill/undotree', config = function() vim.g.undotree_WindowsLayout = 2 end,  cmd = 'UndotreeToggle'}
+  use {'wbthomason/packer.nvim', opt = true} -- Packer can manage itself
+  -- use {'mhinz/vim-startify', config = [[require('config.startify')]]}
+  use {'kyazdani42/nvim-tree.lua', config = [[require('config.nvimtree')]], cmd = 'NvimTreeToggle'}
+  use {'mbbill/undotree', config = [[vim.g.undotree_SetFocusWhenToggle = 1]],  cmd = 'UndotreeToggle'}
+  use {'nvim-telescope/telescope.nvim', config = [[require('config.telescope')]], cmd = 'Telescope', module = 'telescope'}
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
 
   -- Movements
-  use 'tpope/vim-unimpaired'
+  use {'tpope/vim-unimpaired', keys = 'yoc'}
   use 'tpope/vim-eunuch'
   use 'tpope/vim-rsi'
-  use 'ggandor/lightspeed.nvim'
-  use 'mg979/vim-visual-multi'
+  use {'ggandor/lightspeed.nvim'}
+  -- use {'ggandor/lightspeed.nvim', keys = { {'n','s'}, {'n','S'}, {'n','f'}, {'n','F'} }}
+  use {'mg979/vim-visual-multi', keys = '<c-n>' }
 
   -- Syntaxes and such
-  use{'nvim-treesitter/nvim-treesitter',
-    config = function () require('config.treesitter') end,
-    run = ':TSUpdate'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      {'nvim-treesitter/nvim-treesitter-refactor', after = 'nvim-treesitter'},  -- Refactor module for treesitter
+      {'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter'},  -- text-objects module for treesitter
+      {'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter'}, -- context-commentstring module for treesitter
+      {'windwp/nvim-ts-autotag', after = 'nvim-treesitter'}, -- auto complete HTML tags
+    },
+    config = [[require('config.treesitter')]],
+    run = ':TSUpdate',
   }  -- We recommend updating the parsers cmd update
-  use 'nvim-treesitter/nvim-treesitter-refactor'  -- Refactor module for treesitter
-  use 'nvim-treesitter/nvim-treesitter-textobjects'  -- text-objects module for treesitter
-  use 'JoosepAlviste/nvim-ts-context-commentstring' -- context-commentstring module for treesitter
-  use 'windwp/nvim-ts-autotag' -- auto complete HTML tags
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'AndrewRadev/splitjoin.vim' -- gS for splitting & gJ for joining
-  use{'numToStr/Comment.nvim', config = function () require('Comment').setup() end} -- Comment plugins with treesitter support
-  use{'windwp/nvim-autopairs', config = function () require('config.autopairs') end} -- autopairs plugin
-  use{'andymass/vim-matchup', config = function () vim.g.matchup_matchparen_deferred = 1 end} -- Replace default `matchit` & `matchparen`
+  use {'tpope/vim-surround', keys = { {'v', 'S'}, 'ys' }}
+  use {'tpope/vim-repeat', keys = '.'}
+  use {'AndrewRadev/splitjoin.vim', cmd = { 'SplitjoinJoin', 'SplitjoinSplit' }} -- gS for splitting & gJ for joining
+  use {'numToStr/Comment.nvim', config = [[require('Comment').setup()]]} -- Comment plugins with treesitter support
+  use {'windwp/nvim-autopairs', config = [[require('config.autopairs')]]} -- autopairs plugin
+  use {'andymass/vim-matchup', config = 'vim.g.matchup_matchparen_deferred = 1'} -- Replace default `matchit` & `matchparen`
 
   -- LSP & diagnostics
   use 'kevinhwang91/nvim-bqf'
-  use{'neovim/nvim-lspconfig', config = function () require('config.lsp') end}
-  use{'folke/trouble.nvim', config = function () require('config.trouble') end}
+  use 'nvim-lua/lsp-status.nvim'
+  use 'folke/lsp-colors.nvim'
+  use {'simrat39/symbols-outline.nvim', cmd = 'SymbolsOutline'}
+  use {'neovim/nvim-lspconfig', config = [[require('config.lsp')]]}
+  use {'folke/trouble.nvim', config = [[require('config.trouble')]]}
 
   -- Completions
-  use 'github/copilot.vim'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/cmp-copilot'
-  use 'hrsh7th/cmp-nvim-lsp-document-symbol'
-  use 'andersevenrud/cmp-tmux'
-  use{'tzachar/cmp-tabnine', run = './install.sh', config = function () require('config.tabnine') end}
-  use{'hrsh7th/nvim-cmp', config = function () require('config.cmp') end}
+  use {
+    'hrsh7th/nvim-cmp',
+    -- after = 'nvim-treesitter',
+    config = [[require('config.cmp')]],
+    -- event = 'InsertEnter *',
+    requires = {
+      'nvim-treesitter/nvim-treesitter',
+      'hrsh7th/cmp-nvim-lsp',
+      { 'github/copilot.vim', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lua', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-copilot', after = 'nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp-document-symbol', after = 'nvim-cmp' },
+      { 'andersevenrud/cmp-tmux', after = 'nvim-cmp' },
+      { 'saadparwaiz1/cmp_luasnip', after = 'nvim-cmp' },
+      { 'tzachar/cmp-tabnine', run = './install.sh', config = [[require('config.tabnine')]], after = 'nvim-cmp' }
+    },
+  }
 
   -- VCS
-  use{'lewis6991/gitsigns.nvim', config = function () require('config.gitsigns') end}
-  use{'TimUntersberger/neogit', config = function () require('config.neogit') end}
-  use{'sindrets/diffview.nvim', config = function () require('config.diffview') end}
+  use {'lewis6991/gitsigns.nvim', config = [[require('config.gitsigns')]]}
+  use {'TimUntersberger/neogit', config = [[require('config.neogit')]], cmd = 'Neogit'}
+  use {'sindrets/diffview.nvim', config = [[require('config.diffview')]], cmd = 'DiffviewOpen'}
 
   -- Snippets
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
+  use {'L3MON4D3/LuaSnip', after = 'nvim-cmp'}
   use 'rafamadriz/friendly-snippets'
 
   -- Colorschemes
-  use 'folke/lsp-colors.nvim'
-  use 'gruvbox-community/gruvbox'
-  use 'arcticicestudio/nord-vim'
-  use 'rakr/vim-one'
-  use 'EdenEast/nightfox.nvim'
-  use 'NLKNguyen/papercolor-theme'
-  -- use{'catppuccin/nvim', config = function () require('config.catppuccin') end}
-  use{'kuntau/ayu-vim', branch = 'italic'}
+  use {'kuntau/ayu-vim', branch = 'italic', opt = true}
+  use {'gruvbox-community/gruvbox', opt = true} -- Slowwww
+  use {'arcticicestudio/nord-vim', opt = true}
+  use {'rakr/vim-one', opt = true}
+  use {'EdenEast/nightfox.nvim', opt = true}
+  use {'NLKNguyen/papercolor-theme', opt = true}
+  use {'folke/tokyonight.nvim', opt = true}
+  use {'catppuccin/nvim', opt = true}
+  use {'rebelot/kanagawa.nvim'} -- tokyonight + gruvbox
 
   -- Misc bundle
   use 'christoomey/vim-tmux-navigator'
   use 'junegunn/vim-easy-align'
-  use{'dstein64/vim-startuptime', cmd = 'StartupTime'} -- startup time benachmarking
-  use{'ahmedkhalf/project.nvim', config = function () require('project_nvim').setup() end}
+  use {'dstein64/vim-startuptime', config = 'vim.g.startuptime_tries = 10', cmd = 'StartupTime'} -- startup time benachmarking
+  use {'ahmedkhalf/project.nvim', config = [[require('project_nvim').setup()]]}
 
   -- UI & UX
   use 'p00f/nvim-ts-rainbow'
   use 'kyazdani42/nvim-web-devicons' -- for file icons
   use 'onsails/lspkind-nvim' -- LSP completion menu icons
-  use{'lukas-reineke/indent-blankline.nvim', config = function () require('config.indent') end}
-  use{'norcalli/nvim-colorizer.lua', config = function () require('colorizer').setup() end}
-  use{'famiu/bufdelete.nvim', cmd = 'Bdelete'} -- improve :bdelete experience
-  use{'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
+  use {'lukas-reineke/indent-blankline.nvim', config = [[require('config.indent')]]}
+  use {'norcalli/nvim-colorizer.lua', config = [[require('colorizer').setup()]]}
+  use {'famiu/bufdelete.nvim', cmd = 'Bdelete'} -- improve :bdelete experience
 
   -- StatusLine, bufferline & tabline
-  use 'edkolev/tmuxline.vim' -- Tmux statusline
-  use{'kdheepak/tabline.nvim', config = function () require('config.tabline') end}
-  use{'nvim-lualine/lualine.nvim', config = function() require('config.statusline') end} -- statusline
+  use {'edkolev/tmuxline.vim', cmd = 'Tmuxline'} -- Tmux statusline
+  use {'kdheepak/tabline.nvim', config = [[require('config.tabline')]]}
+  use {'nvim-lualine/lualine.nvim', config = [[require('config.statusline')]]} -- statusline
   -- use('famiu/feline.nvim') -- statusline
   -- use('windwp/windline.nvim') -- statusline
 
@@ -121,7 +139,7 @@ return packer.startup({function(use)
   end
 end,
   config = {
-    display = { open_fn = require("packer.util").float},
+    -- display = { open_fn = require("packer.util").float},
     -- Move to lua dir so impatient.nvim can cache it
     compile_path = fn.stdpath('config')..'/lua/packer_compiled.lua'
   }
