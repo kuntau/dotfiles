aff5b8385e4092012a088d93b500c2bbbf8a0e8a
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-28 00:38:48 +0800

    fix(nvim): Add error checking while requiring `impatient`

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,12 @@
 -- init.lua --
 
-require('impatient') -- NOS!! https://github.com/lewis6991/impatient.nvim
+-- NOS!! https://github.com/lewis6991/impatient.nvim
+local ok, impatient = pcall(require, 'impatient')
+if not ok then
+  print("failed to require 'impatient'")
+  return
+end
+
 require('options')
 require('packer_compiled')
 require('plugins')
