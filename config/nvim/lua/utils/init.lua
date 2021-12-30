850045df4ebec7c9500200576ac27cdaeae5db7b
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-31 04:33:12 +0800

    refactor(nvim.util): Better inspect global.

--- config/nvim/lua/utils/init.lua
+++ config/nvim/lua/utils/init.lua
@@ -4,8 +4,15 @@ local fn = vim.fn
 local cmd = vim.cmd
 
 -- Global helpers
-_G.I = function (v)
-  return vim.inspect(v)
+_G.I = function (...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, '\n'))
+  return ...
 end
 
 _G.P = function (v)
