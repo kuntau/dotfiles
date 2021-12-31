8fccd904c045a41fb48c602f6f154b5610e9176a
Nizamuddin Sulieman <kuntau17@gmail.com>
2021-12-31 11:06:01 +0800

    chore(nvim): Reorganize `indent_blankline` configs

--- config/nvim/lua/config/indent.lua
+++ config/nvim/lua/config/indent.lua
@@ -1,24 +1,23 @@
 -- indent_blankline.nvim configs
 
-vim.g.indent_blankline_filetype_exclude = {
-  'NvimTree',
-  'git',
-  'help',
-  'lspinfo',
-  'markdown',
-  'vim-plug',
-  'startify',
-  'tex',
-  'txt',
-  'startuptime',
-  'checkhealth',
-  'packer',
-}
-vim.g.indent_blankline_use_treesitter = true
-
 require("indent_blankline").setup({
   show_current_context = true,
   show_end_of_line = true,
-  show_first_indent_level = false
-  -- show_current_context_start = true
+  show_first_indent_level = false,
+  indent_blankline_use_treesitter = true,
+  indent_blankline_filetype_exclude = {
+    'NvimTree',
+    'git',
+    'help',
+    'lspinfo',
+    'markdown',
+    'startify',
+    'startuptime',
+    'checkhealth',
+    'packer',
+  },
+  indent_blankline_bufname_exclude = {
+    'terminal',
+    'nofile'
+  },
 })
