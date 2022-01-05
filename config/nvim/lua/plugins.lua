27f7c4bdb29fb2bff072eeea92c46587f01b93d0
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-01-05 09:48:02 +0800

    chore(nvim): Formatting. disable `lspkind` & `lsp-colors`

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -46,17 +46,16 @@ return packer.startup({function(use)
   use { 'mg979/vim-visual-multi', keys = '<c-n>'  }
 
   -- Syntaxes and such
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    requires = {
-      {'nvim-treesitter/nvim-treesitter-refactor',    event = 'InsertEnter *'}, -- Refactor module for treesitter
-      {'nvim-treesitter/nvim-treesitter-textobjects', event = 'InsertEnter *'}, -- text-objects module for treesitter
-      {'JoosepAlviste/nvim-ts-context-commentstring', after = 'Comment.nvim'}, -- context-commentstring module for treesitter
-      {'windwp/nvim-ts-autotag', after = 'nvim-treesitter', ft = {'md','vue','html','jsx','tsx'}}, -- auto complete HTML tags
+  use { 'nvim-treesitter/nvim-treesitter', requires = {
+      { 'nvim-treesitter/nvim-treesitter-refactor',    event = 'InsertEnter *' }, -- Refactor module for treesitter
+      { 'nvim-treesitter/nvim-treesitter-textobjects', event = 'InsertEnter *' }, -- text-objects module for treesitter
+      { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'Comment.nvim' }, -- context-commentstring module for treesitter
+      { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter', ft = {'md','vue','html','jsx','tsx'} }, -- auto complete HTML tags
+      { 'p00f/nvim-ts-rainbow', ft = {'fnl'} },
     },
     config = [[require('config.treesitter')]],
-    run = ':TSUpdate',
-  }  -- We recommend updating the parsers cmd update
+    run = ':TSUpdate' -- We recommend updating the parsers cmd update
+  }
   use { 'tpope/vim-surround', keys = { {'v', 'S'}, 'y', 'c', 'd' } }
   use { 'tpope/vim-repeat', keys = '.' }
   use { 'AndrewRadev/splitjoin.vim', cmd = { 'SplitjoinJoin', 'SplitjoinSplit' } } -- gS for splitting & gJ for joining
@@ -65,21 +64,16 @@ return packer.startup({function(use)
   use { 'andymass/vim-matchup', config = [[require('config.matchup')]], event = 'InsertEnter *' } -- Replace default `matchit` & `matchparen`
 
   -- LSP & diagnostics
-  use { 'onsails/lspkind-nvim' } -- LSP completion menu icons
-  use { 'folke/lsp-colors.nvim' }
-  use { 'kevinhwang91/nvim-bqf' }
+  -- use { 'onsails/lspkind-nvim' } -- LSP completion menu icons
+  -- use { 'folke/lsp-colors.nvim' }
+  use { 'kevinhwang91/nvim-bqf', event = 'QuickFixCmdPre *' }
   use { 'simrat39/symbols-outline.nvim', cmd = 'SymbolsOutline' }
   use { 'neovim/nvim-lspconfig', config = [[require('config.lsp')]] }
   use { 'folke/trouble.nvim', config = [[require('config.trouble')]], cmd = 'Trouble' }
 
   -- Completions
-  use {
-    'hrsh7th/nvim-cmp',
-    after = 'nvim-treesitter',
-    config = [[require('config.cmp')]],
-    event = 'InsertEnter *',
-    requires = {
-      'hrsh7th/cmp-nvim-lsp',
+  use { 'hrsh7th/nvim-cmp', requires = {
+      { 'hrsh7th/cmp-nvim-lsp' },
       { 'github/copilot.vim',       after = 'nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lua',     after = 'nvim-cmp' },
       { 'hrsh7th/cmp-buffer',       after = 'nvim-cmp' },
@@ -91,10 +85,13 @@ return packer.startup({function(use)
       { 'hrsh7th/cmp-nvim-lsp-document-symbol', after = 'nvim-cmp' },
       { 'tzachar/cmp-tabnine', run = './install.sh', config = [[require('config.tabnine')]], after = 'nvim-cmp' }
     },
+    after = 'nvim-treesitter',
+    config = [[require('config.cmp')]],
+    event = 'InsertEnter *',
   }
 
   -- VCS
-  use { 'lewis6991/gitsigns.nvim', config = [[require('config.gitsigns')]] }
+  use { 'lewis6991/gitsigns.nvim', config = [[require('config.gitsigns')]], event = 'InsertEnter *' }
   use { 'TimUntersberger/neogit', config = [[require('config.neogit')]], cmd = 'Neogit' }
   use { 'sindrets/diffview.nvim', config = [[require('config.diffview')]], cmd = 'DiffviewOpen' }
 
@@ -115,12 +112,11 @@ return packer.startup({function(use)
   use { 'rebelot/kanagawa.nvim', cond = true } -- tokyonight + gruvbox
 
   -- Misc bundle
-  use { 'christoomey/vim-tmux-navigator' }
-  use { 'junegunn/vim-easy-align' }
+  use { 'junegunn/vim-easy-align', keys = {{'v','ga'}, 'ga'} }
+  use { 'christoomey/vim-tmux-navigator', cmd = {'TmuxNavigateLeft','TmuxNavigateRight','TmuxNavigateUp','TmuxNavigateDown',} }
   use { 'dstein64/vim-startuptime', config = 'vim.g.startuptime_tries = 5', cmd = 'StartupTime' } -- startup time benachmarking
 
   -- UI & UX
-  use { 'p00f/nvim-ts-rainbow' }
   use { 'kyazdani42/nvim-web-devicons' } -- for file icons
   use { 'lukas-reineke/indent-blankline.nvim', config = [[require('config.indent')]], cmd = 'IndentBlanklineToggle' }
   use { 'norcalli/nvim-colorizer.lua', config = [[require('colorizer').setup()]], ft = {'html', 'vue', 'css', 'jsx', 'tsx', 'scss', 'js', 'ts', 'haml', 'md', 'styl'} }
@@ -139,7 +135,7 @@ return packer.startup({function(use)
   end
 end,
   config = {
-    display = { open_fn = require("packer.util").float},
+    display = { open_fn = require("packer.util").float },
     -- Move to lua dir so impatient.nvim can cache it
     compile_path = fn.stdpath('config')..'/lua/packer_compiled.lua'
   }
