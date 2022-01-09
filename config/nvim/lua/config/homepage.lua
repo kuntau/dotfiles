a29bb4528707980b7dff919750a46901b0753114
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-01-09 09:01:07 +0800

    refactor(nvim): rename `startify.lua` to `homepage.lua`
    This gonna be place for whatever _greeter_ or _dashboard_ configs we
    might use. It's more generic.

--- config/nvim/lua/config/homepage.lua
+++ config/nvim/lua/config/homepage.lua
@@ -33,7 +33,7 @@ end
 
 vim.g.startify_lists = {
   { header = {('   MRU: ' .. cwd())}, type = 'dir' },
-  { header = {'   MRU: Global'},      type = 'files' },
+  -- { header = {'   MRU: Global'},      type = 'files' },
   { header = {'   Sessions'},         type = 'sessions' },
   -- { header = {'   Git modified'},     type = gitModified },
   -- { header = {'   Git untracked'},    type = gitUntracked },
@@ -56,7 +56,7 @@ vim.g.startify_commands = {
   { ch = { 'Check Health', 'checkhealth' }},
 }
 
-vim.g.startify_disable_at_vimenter = 0
+vim.g.startify_disable_at_vimenter = 1
 vim.g.startify_chnage_to_vcs_root = 1
 vim.g.startify_relative_path = 1
 vim.g.startify_session_sort = 1
@@ -66,24 +66,23 @@ vim.g.startify_change_to_dir = 0
 vim.g.startify_update_oldfiles = 0
 vim.g.startify_enable_unsafe = 1
 vim.g.startify_padding_left = 3
-vim.g.startify_files_number = 5
+vim.g.startify_files_number = 10
 vim.g.startify_fortune_use_unicode = 1
 -- vim.g.startify_custom_header = 0
 -- vim.g.startify_custom_header = 'startify#center(startify#fortune#boxed())'
 
-vim.g.startify_custom_footerx = {
-'                                            ▟▙            ',
-'                                            ▝▘            ',
-'    ██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄   ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖',
-'    ██▛▔ ▝██  ██▄▄▄▄██  ██▛▔▔▜██  ▝██  ██▘  ██  ██▛▜██▛▜██',
-'    ██    ██  ██▀▀▀▀▀▘  ██▖  ▗██   ▜█▙▟█▛   ██  ██  ██  ██',
-'    ██    ██  ▜█▙▄▄▄▟▊  ▀██▙▟██▀   ▝████▘   ██  ██  ██  ██',
-'    ▀▀    ▀▀   ▝▀▀▀▀▀     ▀▀▀▀       ▀▀     ▀▀  ▀▀  ▀▀  ▀▀',
-}
-
 vim.g.startify_custom_header = {
-'   ▀██▀─▄███▄─▀██─██▀██▀▀█',
-'   ─██─███─███─██─██─██▄█',
-'   ─██─▀██▄██▀─▀█▄█▀─██▀█',
-'   ▄██▄▄█▀▀▀─────▀──▄██▄▄█',
+'    ██████████████████████████  ██████████████████████████',
+'    ██                      ██  ██                      ██',
+'    ██  ██  ██  ██████████  ██  ██  ██████  ██  ██  ██  ██',
+'    ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██  ██  ██  ██████  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██  ██  ██          ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██████  ██████████  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██              ██  ██  ██      ██  ██  ██  ██  ██',
+'    ██  ██████  ██████  ██  ██  ██  ██████  ██  ██  ██  ██',
+'    ██  ██  ██      ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██████████████████  ██  ██  ██████████████  ██  ██',
+'    ██                      ██  ██                      ██',
+'    ██████████████████████████  ██████████████████████████',
 }
