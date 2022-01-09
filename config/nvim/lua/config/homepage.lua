7e534a880c548d0d44b1a91c2d3f2a0a9cb2bab7
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-01-09 11:40:28 +0800

    refactor(nvim): modularize `homepage.lua`.
    - so we only load what we need. Should be easier to change greeter
      plugins.

--- config/nvim/lua/config/homepage.lua
+++ config/nvim/lua/config/homepage.lua
@@ -1,6 +1,26 @@
 -- vim-startify configs
 
 local utils = require('utils')
+local a     = require('utils.async')
+local ms    = vim.fn.matchstr
+local fnm   = vim.fn.fnamemodify
+local gcwd  = vim.fn.getcwd
+
+local custom_header = {
+'    ██████████████████████████  ██████████████████████████',
+'    ██                      ██  ██                      ██',
+'    ██  ██  ██  ██████████  ██  ██  ██████  ██  ██  ██  ██',
+'    ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██  ██  ██  ██████  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██  ██  ██          ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██████  ██████████  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██              ██  ██  ██      ██  ██  ██  ██  ██',
+'    ██  ██████  ██████  ██  ██  ██  ██████  ██  ██  ██  ██',
+'    ██  ██  ██      ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
+'    ██  ██████████████████  ██  ██  ██████████████  ██  ██',
+'    ██                      ██  ██                      ██',
+'    ██████████████████████████  ██████████████████████████',
+}
 
 local formatter = function (files)
   local results = {}
@@ -22,67 +42,53 @@ local gitCommit = function ()
   -- local files = formatter(utils.gitListCommit())
   local results = {}
   for _, file in ipairs(utils.gitListCommit()) do
-    table.insert(results, { line = vim.fn.matchstr(file, '\\s\\zs.*'), cmd = '!git show ' .. vim.fn.matchstr(file, '^\\x\\+') })
+    table.insert(results, { line = ms(file, '\\s\\zs.*'), cmd = '!git show ' .. ms(file, '^\\x\\+') })
   end
   return results
 end
 
 local cwd = function ()
-  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+  return fnm(gcwd(), ':t')
 end
 
-vim.g.startify_lists = {
-  { header = {('   MRU: ' .. cwd())}, type = 'dir' },
-  -- { header = {'   MRU: Global'},      type = 'files' },
-  { header = {'   Sessions'},         type = 'sessions' },
-  -- { header = {'   Git modified'},     type = gitModified },
-  -- { header = {'   Git untracked'},    type = gitUntracked },
-  -- { header = {'   Git commits'},      type = gitCommit },
-  -- { header = {'   Bookmarks'},        type = 'bookmarks' },
-  { header = {'   Commands'},         type = 'commands' },
-}
+local startify_config = function()
+  vim.g.startify_lists = {
+    { header = {('   MRU: ' .. cwd())}, type = 'dir' },
+    -- { header = {'   MRU: Global'},      type = 'files' },
+    { header = {'   Sessions'},         type = 'sessions' },
+    { header = {'   Git modified'},     type = gitModified },
+    -- { header = {'   Git untracked'},    type = gitUntracked },
+    -- { header = {'   Git commits'},      type = gitCommit },
+    -- { header = {'   Bookmarks'},        type = 'bookmarks' },
+    { header = {'   Commands'},         type = 'commands' },
+  }
+  vim.g.startify_bookmarks = {
+    { z = '~/dotfiles/config/zshrc' },
+  }
+  vim.g.startify_commands = {
+    { tp = { 'Projects', 'Telescope projects' }},
+    { to = { 'Old files', 'Telescope oldfiles' }},
+    { gs = { 'Git status', 'Telescope git_status' }},
+    { gc = { 'Git commits', 'Telescope git_commits' }},
+    { ps = { 'Packer Sync', 'PackerSync' }},
+    { pc = { 'Packer Compile', 'PackerCompile' }},
+    { ch = { 'Check Health', 'checkhealth' }},
+  }
+  vim.g.startify_disable_at_vimenter = 0
+  vim.g.startify_chnage_to_vcs_root = 1
+  vim.g.startify_relative_path = 1
+  vim.g.startify_session_sort = 1
+  vim.g.startify_session_persistence = 1
+  vim.g.startify_session_autoload = 1
+  vim.g.startify_change_to_dir = 0
+  vim.g.startify_update_oldfiles = 0
+  vim.g.startify_enable_unsafe = 1
+  vim.g.startify_padding_left = 3
+  vim.g.startify_files_number = 10
+  vim.g.startify_fortune_use_unicode = 1
+  vim.g.startify_custom_header = custom_header
+end
 
-vim.g.startify_bookmarks = {
-  { z = '~/dotfiles/config/zshrc' },
-}
-
-vim.g.startify_commands = {
-  { tp = { 'Projects', 'Telescope projects' }},
-  { to = { 'Old files', 'Telescope oldfiles' }},
-  { gs = { 'Git status', 'Telescope git_status' }},
-  { gc = { 'Git commits', 'Telescope git_commits' }},
-  { ps = { 'Packer Sync', 'PackerSync' }},
-  { pc = { 'Packer Compile', 'PackerCompile' }},
-  { ch = { 'Check Health', 'checkhealth' }},
-}
-
-vim.g.startify_disable_at_vimenter = 1
-vim.g.startify_chnage_to_vcs_root = 1
-vim.g.startify_relative_path = 1
-vim.g.startify_session_sort = 1
-vim.g.startify_session_persistence = 1
-vim.g.startify_session_autoload = 1
-vim.g.startify_change_to_dir = 0
-vim.g.startify_update_oldfiles = 0
-vim.g.startify_enable_unsafe = 1
-vim.g.startify_padding_left = 3
-vim.g.startify_files_number = 10
-vim.g.startify_fortune_use_unicode = 1
--- vim.g.startify_custom_header = 0
--- vim.g.startify_custom_header = 'startify#center(startify#fortune#boxed())'
-
-vim.g.startify_custom_header = {
-'    ██████████████████████████  ██████████████████████████',
-'    ██                      ██  ██                      ██',
-'    ██  ██  ██  ██████████  ██  ██  ██████  ██  ██  ██  ██',
-'    ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
-'    ██  ██  ██  ██  ██████  ██  ██  ██  ██  ██  ██  ██  ██',
-'    ██  ██  ██  ██          ██  ██  ██  ██  ██  ██  ██  ██',
-'    ██  ██████  ██████████  ██  ██  ██  ██  ██  ██  ██  ██',
-'    ██  ██              ██  ██  ██      ██  ██  ██  ██  ██',
-'    ██  ██████  ██████  ██  ██  ██  ██████  ██  ██  ██  ██',
-'    ██  ██  ██      ██  ██  ██  ██  ██  ██  ██  ██  ██  ██',
-'    ██  ██████████████████  ██  ██  ██████████████  ██  ██',
-'    ██                      ██  ██                      ██',
-'    ██████████████████████████  ██████████████████████████',
+return {
+  startify = startify_config
 }
