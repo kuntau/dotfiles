6c8ed93519e45b24e983c353e7aa1d76e0c64e7f
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-01-17 12:27:44 +0800

    refactor(nvim): modify `plugins` & `init`
    - add command if require `packer_compiled` fail
    - add loading packer commands if < 0.7
    - add plugin `tabout.nvim`
    - add plugin `scratch.vim`
    - add plugin `vim-gist`
    - add plugin `nvim-nu` - ft `nu` support
    - add plugin `FTerm.nvim` - floating terminal
    - add colorscheme `everforest`
    - replace plugin `colorizer` with `hexokinase`
    - remove plugin`auto-session`
    - remove plugin`alpha-nvim`
    - remove plugin`gruvbox`
    - remove automatic `require('plugins')`
    - change packer `git update` commands
    - change lazy loading trigger
    - fix return after `impatient` fail

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,19 +1,23 @@
 -- init.lua --
 
 -- NOS!! https://github.com/lewis6991/impatient.nvim
-local ok, impatient = pcall(require, 'impatient')
-if not ok then
+local impatient_ok, _ = pcall(require, 'impatient')
+if not impatient_ok then
   print("failed to require 'impatient'")
-  return
 end
 -- impatient.enable_profile()
 
+local compiled_ok, _ = pcall(require, 'packer_compiled')
+if not compiled_ok then
+  vim.api.nvim_command [[packadd packer.nvim | lua require('plugins').sync()]]
+  require('plugins')
+end
+
 require('options')
-require('plugins')
-require('packer_compiled')
-require('ui')
 require('mappings')
+require('ui')
 require('events')
+
 if vim.fn.has('nvim-0.7') == 1 then
   require('commands')
 end
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,46 +3,62 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 local packer_bookstrap
+local load_cmds = true
 
 if fn.empty(fn.glob(install_path)) > 0 then
   local packer_repo = 'https://github.com/wbthomason/packer.nvim'
-  packer_bookstrap = fn.system({'git', 'clone', '--depth', '1',packer_repo, install_path})
+  packer_bookstrap = fn.system({'git', 'clone', '--depth', '1', packer_repo, install_path})
 end
 
-vim.cmd 'packadd packer.nvim'
+if fn.has('nvim-0.7') ~= 1 then
+  vim.cmd 'packadd packer.nvim'
+  load_cmds = false
+end
 
-local packer = require("packer")
+local packer = require('packer')
+
+packer.init {
+  disable_commands = load_cmds,
+  display = { open_fn = function ()
+    return require('packer.util').float({ style = 'minimal', border = 'rounded'})
+  end },
+  compile_path = fn.stdpath('config')..'/lua/packer_compiled.lua', -- Move to lua dir so impatient.nvim can cache it
+  git = {
+    cmd = 'git',
+    subcommands = {
+      update = 'pull --ff --progress --rebase=true',
+    }
+  }
+}
 
 return packer.startup({function(use)
 
   -- dev
-  if fn.empty(fn.glob('~/coding/vim/unimpaired.nvim')) == 0 then
-    use { '~/coding/vim/unimpaired.nvim', cond = false }
+  if fn.isdirectory(fn.glob('~/coding/vim/unimpaired.nvim')) == 1 then
+    use { '~/coding/vim/unimpaired.nvim', disable = true }
   end
 
   -- Essentials
   use { 'lewis6991/impatient.nvim' }
   use { 'kuntau/vim-osc52', keys = '<c-c>' } -- Copy & paste across tmux & screen over mosh
-  -- use { 'rmagatti/auto-session', config = [[require('auto-session').setup({auto_session_enabled = false})]] }
   use { 'wbthomason/packer.nvim', opt = true } -- Packer can manage itself
   use { 'kyazdani42/nvim-tree.lua', config = [[require('config.nvimtree')]], cmd = 'NvimTreeToggle' }
   use { 'mbbill/undotree', config = [[vim.g.undotree_SetFocusWhenToggle = 1]],  cmd = 'UndotreeToggle' }
   use { 'nvim-telescope/telescope.nvim', requires = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-telescope/telescope-fzf-native.nvim', config = [[require('telescope').load_extension('fzf')]], run = 'make', after = 'telescope.nvim', },
-      { 'nvim-telescope/telescope-frecency.nvim', config = [[require('telescope').load_extension('frecency')]], requires = 'tami5/sqlite.lua', after = 'telescope.nvim' },
+      { 'nvim-telescope/telescope-frecency.nvim', config = [[require('telescope').load_extension('frecency')]], requires = { 'tami5/sqlite.lua', event = 'BufReadPre' }, after = 'telescope.nvim' },
       { 'ahmedkhalf/project.nvim', after = 'telescope.nvim', config = function () require('project_nvim').setup() require('telescope').load_extension('projects') end, },
     },
     config = [[require('config._telescope')]],
     cmd = 'Telescope',
   }
-  -- use { 'goolord/alpha-nvim', config = [[require('alpha').setup(require('alpha.themes.startify').opts)]] }
   use { 'mhinz/vim-startify', config = [[require('config.homepage').startify()]] }
 
   -- Movements
-  use { 'tpope/vim-unimpaired', keys = {'yo', '[', ']'} }
-  use { 'tpope/vim-rsi', event = 'InsertEnter *' }
-  use { 'ggandor/lightspeed.nvim', keys = { 's', 'S', 'f', 'F', 't', 'T' } }
+  use { 'tpope/vim-unimpaired', event = 'BufReadPost' }
+  use { 'tpope/vim-rsi', event = { 'InsertEnter *', 'CmdlineEnter' } }
+  use { 'ggandor/lightspeed.nvim', config = 'vunmap S', keys = { 's', 'S', 'f', 'F', 't', 'T' } }
   use { 'mg979/vim-visual-multi', keys = '<c-n>'  }
 
   -- Syntaxes and such
@@ -53,7 +69,8 @@ return packer.startup({function(use)
       { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'Comment.nvim' }, -- context-commentstring module for treesitter
       { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter', ft = {'md','vue','html','jsx','tsx'} }, -- auto complete HTML tags
       { 'p00f/nvim-ts-rainbow', after = 'nvim-treesitter', ft = {'fnl'} },
-      { 'romgrk/nvim-treesitter-context', after = 'nvim-treesitter' }
+      { 'romgrk/nvim-treesitter-context', after = 'nvim-treesitter' },
+      { 'abecodes/tabout.nvim', config = [[require('tabout').setup()]], after = 'nvim-treesitter' }
     },
     event = 'BufRead',
     config = [[require('config.treesitter')]],
@@ -67,7 +84,7 @@ return packer.startup({function(use)
   use { 'andymass/vim-matchup', config = [[require('config.matchup')]], event = 'BufReadPost' } -- Replace default `matchit` & `matchparen`
 
   -- LSP & diagnostics
-  use { 'neovim/nvim-lspconfig', config = [[require('lsp')]], event = 'BufReadPre' }
+  use { 'neovim/nvim-lspconfig', config = [[require('lsp')]], event = 'BufReadPre', after = 'cmp-nvim-lsp' }
   use { 'kevinhwang91/nvim-bqf', ft = 'qf' }
   use { 'simrat39/symbols-outline.nvim', cmd = 'SymbolsOutline' }
   use { 'folke/trouble.nvim', config = [[require('config.trouble')]], cmd = 'Trouble' }
@@ -102,52 +119,51 @@ return packer.startup({function(use)
 
   -- Snippets
   use { 'L3MON4D3/LuaSnip', after = 'nvim-cmp', wants = { 'friendly-snippets' } }
-  use { 'rafamadriz/friendly-snippets' }
+  use { 'rafamadriz/friendly-snippets', event = 'BufReadPre' }
 
   -- Colorschemes
   use { 'kuntau/ayu-vim', branch = 'italic', opt = true }
-  use { 'gruvbox-community/gruvbox', opt = true } -- Slowwww
   use { 'arcticicestudio/nord-vim', opt = true }
   use { 'NLKNguyen/papercolor-theme', opt = true }
+  use { 'sainnhe/everforest', config = 'vim.g.everforest_background = hard', opt = true }
   use { 'rakr/vim-one', opt = true }
   use { 'catppuccin/nvim', as = 'catppuccin', config = [[require('config.catppuccin')]], opt = true }
   use { 'EdenEast/nightfox.nvim', config = [[require('config.nightfox')]], opt = true }
   use { 'marko-cerovac/material.nvim', config = 'vim.g.material_style = "palenight"', opt = true }
   use { 'folke/tokyonight.nvim' }
-  use { 'rebelot/kanagawa.nvim' } -- tokyonight + gruvbox
+  use { 'rebelot/kanagawa.nvim', config = [[require'kanagawa'.setup{dimInactive = true}]] } -- tokyonight + gruvbox
 
   -- Qualify of life
   use { 'junegunn/vim-easy-align', cmd = 'EasyAlign' }
-  use { 'dstein64/vim-startuptime', config = 'vim.g.startuptime_tries = 5', cmd = 'StartupTime' } -- startup time benachmarking
-  use { 'mtth/scratch.vim', cmd = 'Scratch' }
-  use { 'antoinemadec/FixCursorHold.nvim', config = 'vim.g.cursorhold_updatetime = 100' } -- Fix CursorHold,CursorHoldI bug
-  use { 'christoomey/vim-tmux-navigator', config = 'vim.g.tmux_navigator_disable_when_zoomed = 1' }
   use { 'gennaro-tedesco/nvim-peekup', keys = [[""]] }
-  use { 'kazhala/close-buffers.nvim', cmd = 'BDelete'}
+  use { 'numToStr/FTerm.nvim', config = [[require('FTerm').setup({cmd='/usr/local/bin/nu'})]], event = 'BufReadPost' }
+  use { 'kazhala/close-buffers.nvim', cmd = { 'BDelete', 'BWipeout' } }
+  use { 'dstein64/vim-startuptime', config = 'vim.g.startuptime_tries = 5', cmd = 'StartupTime' } -- startup time benachmarking
+  use { 'antoinemadec/FixCursorHold.nvim', config = 'vim.g.cursorhold_updatetime = 100', event = 'BufReadPre' } -- Fix CursorHold,CursorHoldI bug
+  use { 'christoomey/vim-tmux-navigator', config = 'vim.g.tmux_navigator_disable_when_zoomed = 1', event = 'BufReadPost' }
+  use { 'mtth/scratch.vim', config = [[vim.g.scratch_persistence_file = '.scratch']], cmd = { 'Scratch', 'ScratchSelection' } }
+  use { 'mattn/vim-gist', config = [[vim.g.gist_clip_command=pbcopy vim.g.gist_detect_filetype=1]], cmd = { 'Gist' } }
 
   -- UI & UX
   use { 'kyazdani42/nvim-web-devicons' } -- for file icons
-  use { 'lukas-reineke/indent-blankline.nvim', config = [[require('config.indent')]], event = 'BufReadPre' }
-  use { 'norcalli/nvim-colorizer.lua', config = [[require('colorizer').setup()]], ft = {'html', 'vue', 'css', 'jsx', 'tsx', 'scss', 'js', 'ts', 'haml', 'md', 'styl'} }
+  use { 'lukas-reineke/indent-blankline.nvim', config = [[require('config.indent')]], cmd = 'IndentBlanklineEnable' }
+  -- use { 'norcalli/nvim-colorizer.lua', config = [[require('colorizer').setup()]], ft = {'html', 'vue', 'css', 'jsx', 'tsx', 'scss', 'js', 'ts', 'haml', 'md', 'styl'} }
+  use { 'RRethy/vim-hexokinase', run = 'make', event = 'BufReadPost' }
   use { 'norcalli/nvim-terminal.lua', config = [[require('terminal').setup()]], ft = 'terminal' }
   use { 'edluffy/specs.nvim', config = [[require('config.cursor')]], event = 'BufReadPre' }
-  use { 'anuvyklack/pretty-fold.nvim', config = [[require('config.fold')]] }
+  use { 'anuvyklack/pretty-fold.nvim', config = [[require('config.fold')]], event = 'BufReadPre' }
 
   -- StatusLine, bufferline & tabline
   use { 'edkolev/tmuxline.vim', cmd = 'Tmuxline' } -- Tmux statusline
-  use { 'nvim-lualine/lualine.nvim', config = [[require('config.statusline')]] } -- statusline
+  use { 'nvim-lualine/lualine.nvim', config = [[require('config.statusline')]], event = 'User StartifyReady' } -- statusline
 
   -- Language specifics
   use { 'SidOfc/mkdx', ft = 'markdown', config = [[require('config.markdown')]] }
+  -- use { 'LhKipp/nvim-nu', ft = 'nu', after = 'nvim-treesitter' } -- nu shell systax, TSInstall nu
 
   -- automatically setup configurations after cloning packer
   if packer_bookstrap then
     packer.sync()
   end
 end,
-  config = {
-    display = { open_fn = function () return require("packer.util").float({ style = 'minimal', border = 'rounded'}) end },
-    -- Move to lua dir so impatient.nvim can cache it
-    compile_path = fn.stdpath('config')..'/lua/packer_compiled.lua'
-  }
 })
