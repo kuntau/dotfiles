7cc8b378233bf5efef94f7b420a3f27d38875bbb
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-01-19 05:25:16 +0800

    chore(wezterm): update configs
    - use fallback fonts
    - update mappings
    - use _ugly_ tabs
    - zoom pane

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -10,12 +10,16 @@ return {
   -- Fonts
   -- font = wezterm.font("Ligalex Mono")                                , -- [IBM Plex Mono]
   -- font = wezterm.font("BlexMono Nerd Font Mono")                     , -- [IBM Plex Mono]
-  font = wezterm.font("OperatorMonoLigaturized Nerd Font")           , -- [Operator Mono]
   -- font = wezterm.font("JetBrains Mono")                              , -- [JetBrains Mono]
   -- font = wezterm.font("JetBrains Mono", {weight="Bold",italic=true}) , -- [JetBrains Mono] Name with parameters
+  -- font = wezterm.font("OperatorMonoLigaturized Nerd Font")           , -- [Operator Mono]
+  font = wezterm.font_with_fallback({
+    "OperatorMonoLigaturized Nerd Font",
+    "BlexMono Nerd Font Mono"
+  }),
 
-  font_size = 13.0                                                        , -- [12.0]
-  font_antialias  = 'Subpixel',
+  font_size = 13.0,
+  -- font_antialias  = 'Subpixel',
   freetype_load_target = 'HorizontalLcd',
 
   -- Basics
@@ -60,6 +64,8 @@ return {
   window_decorations = 'RESIZE', -- 'TITLE', 'RESIZE', 'NONE'
   exit_behavior = 'CloseOnCleanExit',
   window_close_confirmation = 'NeverPrompt',
+  unzoom_on_switch_pane = false,
+  use_fancy_tab_bar = false,
   skip_close_confirmation_for_processes_named = {
     'bash', 'sh', 'zsh', 'fish', 'tmux', 'nu'
   },
@@ -78,13 +84,22 @@ return {
     { key='v', mods='LEADER', action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
     { key='s', mods='LEADER', action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
     { key='x', mods='LEADER', action=wezterm.action{CloseCurrentPane={confirm=true}} },
+
     { key="h", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"} },
     { key="l", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"} },
     { key="k", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"} },
     { key="j", mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"} },
+
+    -- { key="h", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Left"} },
+    -- { key="l", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Right"} },
+    -- { key="k", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Up"} },
+    -- { key="j", mods="CTRL", action=wezterm.action{ActivatePaneDirection="Down"} },
+
     { key="H", mods="LEADER", action=wezterm.action{AdjustPaneSize={"Left", 5}} },
     { key="J", mods="LEADER", action=wezterm.action{AdjustPaneSize={"Down", 5}} },
     { key="K", mods="LEADER", action=wezterm.action{AdjustPaneSize={"Up", 5}} },
     { key="L", mods="LEADER", action=wezterm.action{AdjustPaneSize={"Right", 5}} },
+    { key="z", mods="LEADER", action='TogglePaneZoomState' },
+    { key="l", mods="LEADER|CTRL", action=wezterm.action{ClearScrollback='ScrollbackAndViewport'} },
   }
 }
