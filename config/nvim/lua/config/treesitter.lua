521517d8336366682fb8b5b7a30e31c8af33bb01
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-05-12 08:00:07 +0800

    chore(nvim): update treesitter textsubject config

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -103,9 +103,11 @@ require('nvim-treesitter.configs').setup({
   },
   textsubjects = {
     enable = true,
+    prev_selection = ',',
     keymaps = {
       ['.'] = 'textsubjects-smart',
       [';'] = 'textsubjects-container-outer',
+      ['i;'] = 'textsubjects-container-inner',
     }
   },
   context_commentstring = {
