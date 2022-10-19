cd8069a93b955491a787af118f0b0dcef50a0958
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-10-19 13:05:56 +0800

    chore(nvim): update/fix configs

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -6,6 +6,15 @@ local debug = false
 -- nvim_create_user_command({name}, {command}, {*opts})
 local cmd = vim.api.nvim_create_user_command
 
+-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
+vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod     = 'expr'
+    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+  end
+})
+
 -- Packer commands
 cmd('Pac', function (cmd)
   vim.cmd 'packadd packer.nvim'
--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -63,7 +63,7 @@ end
 
 -- Setup lspconfig with snippet support
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
