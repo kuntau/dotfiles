8afe475f570313259acdc23459e9c55cc1d5363e
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-11-23 23:08:15 +0800

    fix(nvim): fix `wezterm` configs & color schemes not working

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -42,28 +42,14 @@ return {
   term = 'wezterm',
 
   -- Advanced options
-  enbale_kitty_graphics = true,
+  enable_kitty_graphics = true,
 
   -- Colors
-  color_scheme      = 'wezterm_tokyonight_storm'    , -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
-  colors            = {           -- color_scheme takes precedence over these
-    foreground      = 'silver'  , -- [silver] The default text color
-    background      = 'black'   , -- [black]  The default background color
-    cursor_bg       = '#52ad70' , -- [#52ad70] Overrides the cell background color when the current cell is occupied by the cursor and the cursor style is set to Block
-    cursor_fg       = 'black'   , -- [black]   Overrides the text color when the current cell is occupied by the cursor
-    cursor_border   = '#52ad70' , -- [#52ad70] Specifies the border color of the cursor when the cursor style is set to Block, of the color of the vertical or horizontal bar when the cursor style is set to Bar or Underline.
-    selection_fg    = 'black'   , -- [black]   The foreground color of selected text
-    selection_bg    = '#fffacd' , -- [#fffacd] The background color of selected text
-    scrollbar_thumb = '#222222' , -- [#222222] The color of the scrollbar "thumb"; the portion that represents the current viewport
-    split           = '#444444' , -- [#444444] The color of the split lines between panes
-  --   ansi         = {"black", "maroon", "green", "olive", "navy", "purple", "teal", "silver"},
-  --   brights      = {"grey", "red", "lime", "yellow", "blue", "fuchsia", "aqua", "white"},
-  --   indexed      = {[136]="#af8700"} , -- Arbitrary colors of the palette in the range from 16 to 255
-  },
+  color_scheme      = 'tokyonight_moon'    , -- full list @ wezfurlong.org/wezterm/colorschemes/index.html
 
   -- Appearance
-  initial_column = 500,
-  initial_row = 300,
+  initial_cols = 500,
+  initial_rows = 300,
   line_height = 1.0,
   window_background_opacity     = 1.0   , -- [1.0] alpha channel value with floating point numbers in the range 0.0 (meaning completely translucent/transparent) through to 1.0 (meaning completely opaque)
   enable_tab_bar                = true  , -- [true]
