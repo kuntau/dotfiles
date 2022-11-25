e595d438cc0b03264906084d470b626eefbdb7eb
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-11-25 12:34:25 +0800

    fix(nvim.lsp): fix server_capabilities name
    Welp... got the memo wrong in 4ad36909768ac909b1f9d0144a0cdce96700c6fb
    
    Reference ServerCapabilities provider name in [specs](https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/)

--- config/nvim/lua/lsp/handler.lua
+++ config/nvim/lua/lsp/handler.lua
@@ -6,19 +6,19 @@ local dbgi = require('utils.logger').dbgi
 local debug = false
 
 -- Set default sign
-local setup = function(bufnr, resolved_capabilities)
+local setup = function(bufnr, server_capabilities)
   if vim.tbl_isempty(vim.fn.sign_getdefined('CodeActionSign')) then
     vim.fn.sign_define('CodeActionSign', { text = "💡", texthl = "LspDiagnosticsDefaultInformation" })
   end
 
-  if resolved_capabilities.document_highlight then
+  if server_capabilities.documentHighlightProvider then
     autocmd('lsp_high', [[CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()]], true)
     autocmd('lsp_cref', [[CursorMoved <buffer> lua vim.lsp.buf.clear_references()]], true)
   end
 
-  if resolved_capabilities.code_action then
+  if server_capabilities.codeActionProvider then
     return
-    autocmd('lsp_coda', [[CursorHold,CursorHoldI <buffer> lua require('lsp.handler').code_action_listener()]], true)
+    -- autocmd('lsp_coda', [[CursorHold,CursorHoldI <buffer> lua require('lsp.handler').code_action_listener(bufnr)]], true)
   end
 
   autocmd('lsp_diag', [[CursorHold <buffer> lua vim.diagnostic.open_float({focusable=false, close_events={'InsertEnter', 'CursorMoved'}})]], true)
@@ -28,7 +28,7 @@ local setup = function(bufnr, resolved_capabilities)
 end
 
 local check_capabilities = function (feature)
-  local clients = vim.lsp.buf_get_clients(0)
+  local clients = vim.lsp.buf_get_active_clients(0)
   local supported = false
 
   for _, client in pairs(clients) do
@@ -39,11 +39,11 @@ local check_capabilities = function (feature)
 end
 
 local code_action_listener = function()
-  local context = { diagnostics = lsp.diagnostic.get_line_diagnostics() }
+  local context = { diagnostics = vim.lsp.diagnostic.get() }
   local params = lsp.util.make_range_params()
 
   params.context = context
-  lsp.buf_request(0, 'textDocument/codeAction', params, function (err, _, result)
+  lsp.buf_request(0, 'textDocument/codeAction', params, function (_, _, result)
     if debug then dbgi(result) end
     -- autocmd('lsp_coda', [[CursorHold,CursorHoldI <buffer> lua require('lsp.handler').code_action()]], true)
   end)
