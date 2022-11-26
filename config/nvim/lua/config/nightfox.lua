61c7b5d3eeafc70afe1a0ab314e1d7250823593d
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-11-26 12:07:23 +0800

    style(nvim): use nightfox & update it's configs

--- config/nvim/lua/config/nightfox.lua
+++ config/nvim/lua/config/nightfox.lua
@@ -3,23 +3,24 @@
 local nf = require("nightfox")
 
 nf.setup({
-  fox = "nightfox", -- Which fox style should be applied
-  alt_nc = true, -- Non current window bg to alt color see `hl-NormalNC`
-  terminal_colors = true, -- Configure the colors used when opening :terminal
-  styles = {
-    comments = "italic", -- Style that is applied to comments: see `highlight-args` for options
-    functions = "bold,italic", -- Style that is applied to functions: see `highlight-args` for options
-    keywords = "bold", -- Style that is applied to keywords: see `highlight-args` for options
-    -- strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
-    -- variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
-  },
-  inverse = {
-    match_paren = false, -- Enable/Disable inverse highlighting for match parens
-    visual = false, -- Enable/Disable inverse highlighting for visual selection
-    search = false, -- Enable/Disable inverse highlights for search highlights
-  },
-  -- colors = {}, -- Override default colors
-  -- hlgroups = {}, -- Override highlight groups
+  options = {
+    dim_inactive = true, -- Non current window bg to alt color see `hl-NormalNC`
+    terminal_colors = true, -- Configure the colors used when opening :terminal
+    styles = {
+      comments = "italic", -- Style that is applied to comments: see `highlight-args` for options
+      functions = "bold,italic", -- Style that is applied to functions: see `highlight-args` for options
+      keywords = "bold", -- Style that is applied to keywords: see `highlight-args` for options
+      -- strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
+      -- variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
+    },
+    inverse = {
+      match_paren = false, -- Enable/Disable inverse highlighting for match parens
+      visual = false, -- Enable/Disable inverse highlighting for visual selection
+      search = false, -- Enable/Disable inverse highlights for search highlights
+    },
+    -- colors = {}, -- Override default colors
+    -- hlgroups = {}, -- Override highlight groups
+  }
 })
 
 nf.load()
--- config/nvim/lua/ui.lua
+++ config/nvim/lua/ui.lua
@@ -26,7 +26,7 @@ if utils.isGui() then -- running in GUI
   end
   vim.cmd 'colo tokyonight'
 else -- running in terminal
-  vim.cmd 'colo tokyonight'
+  vim.cmd 'colo nightfox'
 end
 
 -- Colorscheme
