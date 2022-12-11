54bdf3db4e7f5ec4a5b2e891c2279eaa5171a08e
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-12 01:15:47 +0800

    fix(nvim): fix comment.vim always use block comment
    There's new code to integrate `Comment.nvim` with
    `ts_context_commentstring`.
    
    https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim

--- config/nvim/lua/config/comment.lua
+++ config/nvim/lua/config/comment.lua
@@ -1,19 +1,5 @@
 -- configs for Comment.nvim
 
- require('Comment').setup({
-  pre_hook = function(ctx)
-    local commnet_utils = require('Comment.utils')
-
-    local location = nil
-    if ctx.ctype == commnet_utils.ctype.block then
-      location = require('ts_context_commentstring.utils').get_cursor_location()
-    elseif ctx.cmotion == commnet_utils.cmotion.v or ctx.cmotion == commnet_utils.cmotion.V then
-      location = require('ts_context_commentstring.utils').get_visual_start_location()
-    end
-
-    return require('ts_context_commentstring.internal').calculate_commentstring {
-      key = ctx.ctype == commnet_utils.ctype.line and '__default' or '__multiline',
-      location = location,
-    }
-  end,
- })
+require('Comment').setup({
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+})
