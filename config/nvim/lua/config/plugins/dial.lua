05c257553a231efe6f5394f39ef169666fa53534
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-23 09:04:40 +0800

    feat(nvim): add `dial.nvim` plugin

---
+++ config/nvim/lua/config/plugins/dial.lua
@@ -0,0 +1,19 @@
+return {
+  "monaqa/dial.nvim",
+  keys = { '<c-a>', '<c-x>' },
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.bool,
+        augend.semver.alias.semver,
+      },
+    })
+
+    vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+    vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+  end
+}
