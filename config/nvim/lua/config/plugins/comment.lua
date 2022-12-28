a52f7831fc4d551a7b1f074b1fa9bd1a41008e37
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-29 02:29:50 +0800

    refactor(nvim): move `Comment.nvim` to own file
    - enable `ts-context-commentstring` hook
    - add `ts-context-commentstring` as dependencies

---
+++ config/nvim/lua/config/plugins/comment.lua
@@ -0,0 +1,15 @@
+-- configs for Comment.nvim
+-- Comment plugins with treesitter support
+
+local config = function()
+  require('Comment').setup({
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  })
+end
+
+return {
+  'numToStr/Comment.nvim',
+  keys = { 'gb', 'gc' },
+  dependencies = 'JoosepAlviste/nvim-ts-context-commentstring', -- context-commentstring module for treesitter
+  config = config,
+}
