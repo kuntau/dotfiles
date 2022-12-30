5a3ed8cd65979899994d0727f7b917cb6379c494
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-31 03:22:11 +0800

    feat(nvim): add theme `oxocarbon` & `doom-one`
    - fix/add `catppuccin` configs
    - housekeeping theme

--- config/nvim/lua/config/plugins/theme.lua
+++ config/nvim/lua/config/plugins/theme.lua
@@ -2,20 +2,66 @@
 
 local config_catpuccin = function()
   require('catppuccin').setup({
+    flavour = 'macchiato',
     term_colors = true,
+    background = {
+      light = 'latte',
+      dark  = 'macchiato',
+    },
+    dim_inactive = {
+      enabled     = true,
+      shade       = 'dark',
+      perccentage = 0.15,
+    },
     styles = {
-      comments = "italic",
-      functions = "italic",
-      keywords = "italic",
-      strings = "NONE",
-      variables = "NONE",
+      comments    = { "italic" },
+      conditional = { "italic" },
+      functions   = { "italic" },
+      keywords    = { "italic" },
+      strings     = {},
+      variables   = {},
     },
     integrations = {
-      lightspeed  = true,
-      lsp_trouble = true,
-      neogit      = true,
-      ts_rainbow  = true,
-    }
+      cmp                = true,
+      gitsigns           = true,
+      leap               = true,
+      lsp_trouble        = true,
+      markdown           = true,
+      mason              = true,
+      noice              = true,
+      notify             = true,
+      nvimtree           = true,
+      symbols_outline    = true,
+      telescope          = true,
+      treesitter         = true,
+      treesitter_context = true,
+      ts_rainbow         = true,
+      which_key          = true,
+    },
+    -- Special integrations, see https://github.com/catppuccin/nvim#special-integrations
+    dap = {
+      enabled   = true,
+      enable_ui = true,
+    },
+    indent_blankline = {
+      enabled = true,
+      colored_indent_levels = true,
+    },
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors      = { "italic" },
+        hints       = { "italic" },
+        warnings    = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors      = { "underline" },
+        hints       = { "underline" },
+        warnings    = { "underline" },
+        information = { "underline" },
+      },
+    },
   })
 end
 
@@ -24,12 +70,12 @@ local config_nightfox = function()
 
   nf.setup({
     options = {
-      dim_inactive = true, -- Non current window bg to alt color see `hl-NormalNC`
+      dim_inactive    = true, -- Non current window bg to alt color see `hl-NormalNC`
       terminal_colors = true, -- Configure the colors used when opening :terminal
       styles = {
-        comments = "italic", -- Style that is applied to comments: see `highlight-args` for options
+        comments  = "italic", -- Style that is applied to comments: see `highlight-args` for options
         functions = "bold,italic", -- Style that is applied to functions: see `highlight-args` for options
-        keywords = "bold", -- Style that is applied to keywords: see `highlight-args` for options
+        keywords  = "bold", -- Style that is applied to keywords: see `highlight-args` for options
         -- strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
         -- variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
       },
@@ -48,14 +94,16 @@ end
 
 return {
   -- Colorschemes
-  { 'kuntau/ayu-vim', branch = 'italic' },
-  { 'arcticicestudio/nord-vim' },
-  { 'NLKNguyen/papercolor-theme' },
-  { 'sainnhe/everforest', config = function() vim.g.everforest_background = 'hard' end },
-  { 'rakr/vim-one' },
-  { 'catppuccin/nvim', name = 'catppuccin', config = true },
+  'NLKNguyen/papercolor-theme',
+  'arcticicestudio/nord-vim',
+  'folke/tokyonight.nvim',
+  'nyoom-engineering/oxocarbon.nvim', -- IBM carbon inspired
+  'rakr/vim-one',
   { 'EdenEast/nightfox.nvim', config = config_nightfox },
+  { 'NTBBloodbath/doom-one.nvim', config = true },
+  { 'catppuccin/nvim', name = 'catppuccin', config = config_catpuccin },
+  { 'kuntau/ayu-vim', branch = 'italic' },
   { 'marko-cerovac/material.nvim', config = function() vim.g.material_style = "palenight" end },
-  { 'folke/tokyonight.nvim' },
   { 'rebelot/kanagawa.nvim', config = { dimInactive = true, globalStatus = true } }, -- tokyonight + gruvbox
+  { 'sainnhe/everforest', config = function() vim.g.everforest_background = 'hard' end },
 }
