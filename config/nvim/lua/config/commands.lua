8b7cd7480dd42a9887fcec6e5d9ba64de4cda8ab
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-01 19:22:50 +0800

    feat(nvim): add debugging plugins `dap`, `dap-ui` & `sniprun`
    add `one-small-step-for-vimkind` for debugging lua.
    add commands `DebugStart`, `DebugStop` & `DebugToggle`

--- config/nvim/lua/config/commands.lua
+++ config/nvim/lua/config/commands.lua
@@ -6,7 +6,13 @@ local debug = false
 -- nvim_create_user_command({name}, {command}, {*opts})
 local add_cmd = vim.api.nvim_create_user_command
 
+-- Debugging/DAP
+add_cmd('DebugToggle', function() require('dapui').toggle('tray') end, { nargs = 0, desc = 'Toggle Debugging' })
+add_cmd('DebugStart',  function() require('dapui').start('tray') end,  { nargs = 0, desc = 'Start Debugging' })
+add_cmd('DebugStop',   function() require('dapui').stop('tray') end,   { nargs = 0, desc = 'Stop Debugging' })
+
 add_cmd('YankOSC52', '<Plug>(YankOSC52)', {nargs = 0})
+
 -- FTerm
 add_cmd('G', function () require("FTerm").scratch({ cmd = 'gitui' }) end, { nargs = 0, desc = "Open `gitui` in floating terminal" })
 add_cmd('Top', function () require("FTerm").scratch({ cmd = 'btop' }) end, { nargs = 0, desc = "Open `top` in floating terminal" })
