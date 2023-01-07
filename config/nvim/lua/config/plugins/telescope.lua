0cf67c1b9050a8c8f5548bc86f783edbcf02274c
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-08 00:52:03 +0800

    feat(nvim): telescope add `smart-history` & `ui-select` extensions.
    remove `fzy-native`
    set `inital_mode = normal`
    set `path_display = truncate` -- perf issue
    
    references:
    https://github.com/nvim-telescope/telescope.nvim/issues/1837
    https://www.reddit.com/r/neovim/comments/ura4vu/telescope_too_slow_for_large_directories/

--- config/nvim/lua/config/plugins/telescope.lua
+++ config/nvim/lua/config/plugins/telescope.lua
@@ -1,99 +1,114 @@
 -- telescope and it's extensions configs
 
-local config = function() require('telescope').setup({
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-    },
-    mappings = {
-      i = {
-        ['jj'] = require('telescope.actions').close,
-        ['<M-.>'] = require('telescope.actions.layout').toggle_preview,
-        ['<M-?>'] = require('telescope.actions').which_key,
-        ["<M-Up>"] = require('telescope.actions').cycle_history_prev,
-        ["<M-Down>"] = require('telescope.actions').cycle_history_next,
-        ['<C-u>'] = require('telescope.actions').results_scrolling_up,
-        ['<C-d>'] = require('telescope.actions').results_scrolling_down,
-        ['<PageUp>'] = require('telescope.actions').preview_scrolling_up,
-        ['<PageDown>'] = require('telescope.actions').preview_scrolling_down,
+local config = function()
+  require('telescope').setup({
+    defaults = {
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
       },
-      n = {
-        ['q'] = require('telescope.actions').close,
-        ['D'] = require('telescope.actions').delete_buffer,
-        ['<M-.>'] = require('telescope.actions.layout').toggle_preview,
-        ['<M-p>'] = require('telescope.actions.layout').cycle_layout_prev,
-        ['<M-n>'] = require('telescope.actions.layout').cycle_layout_next,
-        ['<C-u>'] = require('telescope.actions').results_scrolling_up,
-        ['<C-d>'] = require('telescope.actions').results_scrolling_down,
-        ['<PageUp>'] = require('telescope.actions').preview_scrolling_up,
-        ['<PageDown>'] = require('telescope.actions').preview_scrolling_down,
-      }
-    },
-    file_ignore_patterns = { '^node_modules/', '^.git/', '^vendor/' },
-    layout_strategy      = (require("utils").getWinOrientation)(),
-    path_display         = { 'smart' },
-    selection_strategy   = 'closest',
-    sorting_strategy     = 'ascending',
-    -- layout_strategy      = 'bottom_pane',
-    -- dynamic_preview_title= true,
-    layout_config = {
-      horizontal = {
-        prompt_position = 'top',
+      mappings = {
+        i = {
+          ['jj'] = require('telescope.actions').close,
+          ['<M-.>'] = require('telescope.actions.layout').toggle_preview,
+          ['<M-?>'] = require('telescope.actions').which_key,
+          ['<M-Up>'] = require('telescope.actions').cycle_history_prev,
+          ['<M-Down>'] = require('telescope.actions').cycle_history_next,
+          ['<C-u>'] = require('telescope.actions').results_scrolling_up,
+          ['<C-d>'] = require('telescope.actions').results_scrolling_down,
+          ['<PageUp>'] = require('telescope.actions').preview_scrolling_up,
+          ['<PageDown>'] = require('telescope.actions').preview_scrolling_down,
+        },
+        n = {
+          ['q'] = require('telescope.actions').close,
+          ['D'] = require('telescope.actions').delete_buffer,
+          ['<M-.>'] = require('telescope.actions.layout').toggle_preview,
+          ['<M-p>'] = require('telescope.actions.layout').cycle_layout_prev,
+          ['<M-n>'] = require('telescope.actions.layout').cycle_layout_next,
+          ['<C-u>'] = require('telescope.actions').results_scrolling_up,
+          ['<C-d>'] = require('telescope.actions').results_scrolling_down,
+          ['<PageUp>'] = require('telescope.actions').preview_scrolling_up,
+          ['<PageDown>'] = require('telescope.actions').preview_scrolling_down,
+        },
       },
-      vertical = {
-        prompt_position = 'top',
-      },
-    },
-  },
-  pickers = {
-    builtin = {
-      previewer = false,
-      layout_config = { width = 0.5 },
-    },
-    fd = {
-      previewer = false,
-    },
-    find_files = {
-      previewer = false,
-      no_ignore = false,
-      follow = false,
-      hidden = true,
-    },
-    oldfiles = {
-      only_cwd = true,
-    },
-    buffers = {
-      previewer = false,
+      -- file_ignore_patterns = { '^node_modules/', '^.git/' },
+      layout_strategy = (require('utils').getWinOrientation)(),
+      path_display = { 'truncate' },
+      selection_strategy = 'closest',
+      sorting_strategy = 'ascending',
       initial_mode = 'normal',
-      sort_lastused = true,
-      sort_mru = true,
-      layout_config = { width = 0.4, height = 0.5 },
+      -- layout_strategy      = 'bottom_pane',
+      -- dynamic_preview_title= true,
+      layout_config = {
+        horizontal = {
+          prompt_position = 'top',
+        },
+        vertical = {
+          prompt_position = 'top',
+        },
+      },
+      history = {
+        path = vim.fn.stdpath('data') .. '/databases/telescope_history.db',
+        limit = 100,
+      },
     },
-    git_bcommits = {
-      initial_mode = 'normal'
+    pickers = {
+      builtin = {
+        previewer = false,
+        layout_config = { width = 0.5 },
+      },
+      fd = {
+        initial_mode = 'insert',
+      },
+      find_files = {
+        initial_mode = 'insert',
+        previewer = false,
+        no_ignore = true,
+        follow = true,
+        hidden = true,
+      },
+      oldfiles = {
+        only_cwd = true,
+      },
+      buffers = {
+        previewer = false,
+        sort_lastused = true,
+        sort_mru = true,
+        layout_config = { width = 0.4, height = 0.5 },
+      },
+      colorscheme = {
+        theme = 'ivy',
+        enable_preview = true,
+      },
     },
-    colorscheme = {
-      theme = 'ivy',
-      -- previewer = false,
-      initial_mode = 'normal',
-      enable_preview = true,
-    }
-  },
-  extensions = {
-    project = {
-      base_dirs = {
-        -- { '~/coding', max_depth = 2 },
-        -- { '~/coding/forks', max_depth = 2 }
-      }
-    }
-  }
-})
+    extensions = {
+      ['frecency'] = { initial_mode = 'insert' },
+      ['projects'] = {
+        require('telescope.themes').get_dropdown({
+          layout_config = { width = 0.4, height = 0.5 },
+        }),
+      },
+      ['project'] = {
+        require('telescope.themes').get_dropdown({
+          layout_config = { width = 0.4, height = 0.5 },
+        }),
+        base_dirs = {
+          -- { '~/coding', max_depth = 2 },
+          -- { '~/coding/forks', max_depth = 2 }
+        },
+      },
+      ['ui-select'] = {
+        require('telescope.themes').get_dropdown({
+          layout_config = { width = 0.4, height = 0.5 },
+        }),
+      },
+    },
+  })
 end
 
 return {
@@ -102,16 +117,14 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
-        'nvim-telescope/telescope-fzy-native.nvim',
-        enabled = false,
-        config = function() require('telescope').load_extension('fzy_native') end,
-        build = 'cd "deps/fzy-lua-native" && make',
-      },
-      {
         'nvim-telescope/telescope-fzf-native.nvim',
         -- enabled = false,
         config = function() require('telescope').load_extension('fzf') end,
         build = 'make',
+      },
+      {
+        'nvim-telescope/telescope-smart-history.nvim',
+        config = function() require('telescope').load_extension('smart_history') end,
       },
     },
     dev = false,
@@ -124,17 +137,21 @@ return {
     dependencies = 'tami5/sqlite.lua',
   },
   {
+    'nvim-telescope/telescope-ui-select.nvim',
+    event = 'VeryLazy',
+    config = function() require('telescope').load_extension('ui-select') end,
+  },
+  {
     'nvim-telescope/telescope-project.nvim',
-    config = function()
-      require('telescope').load_extension('project')
-    end,
+    config = function() require('telescope').load_extension('project') end,
   },
   {
     'ahmedkhalf/project.nvim',
+    -- init = function() require('project_nvim').setup() end,
+    -- event = 'VeryLazy',
     config = function()
-      require('telescope').load_extension('projects')
       require('project_nvim').setup()
+      require('telescope').load_extension('projects')
     end,
-    -- init = function() require('project_nvim').setup() end
   },
 }
