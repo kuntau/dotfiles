50d79085998533ce99fa5dfe3786964cb45de190
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-10 11:47:41 +0800

    fix(nvim): change debugging keymaps to avoid overlapping

--- config/nvim/lua/config/plugins/debug.lua
+++ config/nvim/lua/config/plugins/debug.lua
@@ -2,8 +2,8 @@
 
 local config = function()
   local dap, dapui = require('dap'), require('dapui')
-  require('dapui').setup()
 
+  dapui.setup()
   dap.configurations.lua = {
     {
       type = 'nlua',
@@ -22,15 +22,15 @@ local config = function()
 end
 
 local init = function()
-  vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end, { desc = 'Toggle Breakpoint' })
-  vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end, { desc = 'Continue' })
-  vim.keymap.set('n', '<leader>do', function() require('dap').step_over() end, { desc = 'Step Over' })
-  vim.keymap.set('n', '<leader>di', function() require('dap').step_into() end, { desc = 'Step Into' })
-  vim.keymap.set('n', '<leader>dw', function() require('dap.ui.widgets').hover() end, { desc = 'Widgets' })
-  vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end, { desc = 'Repl' })
-  vim.keymap.set('n', '<leader>du', function() require('dapui').toggle({}) end, { desc = 'Dap UI' })
-  vim.keymap.set('n', '<leader>ds', function() require('osv').launch({ port = 8086 }) end, { desc = 'Launch Lua Debugger Server' })
-  vim.keymap.set('n', '<leader>dd', function() require('osv').run_this() end, { desc = 'Launch Lua Debugger' })
+  vim.keymap.set('n', '<leader>ddb', function() require('dap').toggle_breakpoint() end, { desc = 'Toggle Breakpoint' })
+  vim.keymap.set('n', '<leader>ddc', function() require('dap').continue() end, { desc = 'Continue' })
+  vim.keymap.set('n', '<leader>ddo', function() require('dap').step_over() end, { desc = 'Step Over' })
+  vim.keymap.set('n', '<leader>ddi', function() require('dap').step_into() end, { desc = 'Step Into' })
+  vim.keymap.set('n', '<leader>ddw', function() require('dap.ui.widgets').hover() end, { desc = 'Widgets' })
+  vim.keymap.set('n', '<leader>ddr', function() require('dap').repl.open() end, { desc = 'Repl' })
+  vim.keymap.set('n', '<leader>ddu', function() require('dapui').toggle({}) end, { desc = 'Dap UI' })
+  vim.keymap.set('n', '<leader>dds', function() require('osv').launch({ port = 8086 }) end, { desc = 'Launch Lua Debugger Server' })
+  vim.keymap.set('n', '<leader>ddd', function() require('osv').run_this() end, { desc = 'Launch Lua Debugger' })
 end
 
 return {
