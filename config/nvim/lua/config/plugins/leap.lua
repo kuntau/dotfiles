de58b7597b04735c805defb3ea4e3b00168e6b89
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-10 15:07:56 +0800

    chore(nvim): tweak `leap` configs

--- config/nvim/lua/config/plugins/leap.lua
+++ config/nvim/lua/config/plugins/leap.lua
@@ -2,18 +2,21 @@ return {
   {
     'ggandor/leap.nvim',
     event = 'VeryLazy',
-    config = function() require('leap').add_default_mappings() end,
-    opts = {},
+    config = function()
+      local leap = require('leap')
+      leap.add_default_mappings()
+      leap.opts.case_sensitive = true
+      leap.opts.max_phase_one_targets = 5
+      leap.opts.highlight_unlabeled_phase_one_targets = true
+
+      -- vim.api.nvim_create_autocmd('ColorScheme', {
+      --   callback = function ()
+      --     vim.api.nvim_set_hl(0, 'LeapMatch', { fg = 'black', bg = 'white', bold = true })
+      --     vim.api.nvim_set_hl(0, 'LeapLabelPrimary', { fg = 'white', bg = 'red' })
+      --     vim.api.nvim_set_hl(0, 'LeapLabelSecondary', { fg = 'black', bg = 'white' })
+      --     -- vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+      --   end
+      -- })
+    end,
   },
 }
-
---[[ init = function()
-  -- The below settings make Leap's highlighting a bit closer to what you've been used to in Lightspeed.
-  vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-  vim.api.nvim_set_hl(0, 'LeapMatch', {
-    fg = 'white',  -- for light themes, set to 'black' or similar
-    bold = true,
-    nocombine = true,
-  })
-  require('leap').opts.highlight_unlabeled_phase_one_targets = true
-end, ]]
