41abda54d23dec643f252d3036c497b70a2241b1
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-12 03:29:03 +0800

    fix(nvim): fix `autocmd` load order so we can open file directly and have it's autocmd applied.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,14 @@
 -- init.lua --
 
-require('config.options')
 require('config.lazy')
+require('config.options')
 require('config.ui')
+require('config.events')
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
     require('config.mappings')
-    require('config.events')
     require('config.commands')
   end
 })
