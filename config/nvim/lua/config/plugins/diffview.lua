704b0a290d3bd241ebb396a94aa0928f9ac2b500
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-13 11:50:23 +0800

    feat(nvim): add close mapping to `diffview_file_history` panel

--- config/nvim/lua/config/plugins/diffview.lua
+++ config/nvim/lua/config/plugins/diffview.lua
@@ -4,13 +4,16 @@ local opts = {
   keymaps = {
     disable_defaults = false, -- Disable the default keymaps
     view = {
-      { "n", "q", function() require("diffview.config").actions.close() end, { desc = "Close Diffview" } },
+      { 'n', 'q', function() require('diffview.config').actions.close() end, { desc = 'Close Diffview' } },
     },
     file_panel = {
-      { "n", "q", function() vim.cmd("DiffviewClose") end, { desc = "Close Diffview" } },
-      { "n", "<esc>", function() require("diffview.config").actions.close() end, { desc = "Close file panel" } },
+      { 'n', 'q', function() vim.cmd('DiffviewClose') end, { desc = 'Close Diffview' } },
+      { 'n', '<esc>', function() require('diffview.config').actions.close() end, { desc = 'Close file panel' } },
     },
-  }
+    file_history_panel = {
+      { 'n', 'q', function() vim.cmd('DiffviewClose') end, { desc = 'Close Diffview' } },
+    },
+  },
 }
 
 return {
