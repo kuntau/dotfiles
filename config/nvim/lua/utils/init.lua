ef23902c382e9640bcd373d7b3bae158251b58ce
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-16 08:05:57 +0800

    ref(nvim): rewrite mapper and move to `utils.mapper`

--- config/nvim/lua/utils/init.lua
+++ config/nvim/lua/utils/init.lua
@@ -1,6 +1,9 @@
 -- █░█ ▀█▀ █ █░░ █▀
 -- █▄█ ░█░ █ █▄▄ ▄█
 
+local mapper = require('utils.mapper')
+mapper.setup()
+
 -- Global helpers
 _G.PP = function(...)
   if vim.fn.has('nvim-0.7') == 1 then
@@ -36,38 +39,6 @@ local is_gui = function()
   return false
 end
 
-local feedkey =
-function(key, mode) vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true) end
-
----@param mode string enum of ""|n|v|i|o|x
----@param opts table Mapping options.
-local mapper = function(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts and type(opts) == 'table' then
-    for key, value in pairs(opts) do
-      options[key] = value
-    end
-  end
-
-  local bufo = options.buffer
-  local bufnr = (type(bufo) == 'number' and bufo) or (bufo == true and vim.api.nvim_get_current_buf())
-  options.buffer = nil
-
-  if bufnr then
-    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
-  else
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-  end
-end
-
-local map = function(lhs, rhs, opts) mapper('', lhs, rhs, opts) end
-local tmap = function(lhs, rhs, opts) mapper('t', lhs, rhs, opts) end
-local nmap = function(lhs, rhs, opts) mapper('n', lhs, rhs, opts) end
-local vmap = function(lhs, rhs, opts) mapper('v', lhs, rhs, opts) end
-local imap = function(lhs, rhs, opts) mapper('i', lhs, rhs, opts) end
-local xmap = function(lhs, rhs, opts) mapper('x', lhs, rhs, opts) end
-local omap = function(lhs, rhs, opts) mapper('x', lhs, rhs, opts) end
-
 -- Quickclose some pane
 local quick_close_pane = function()
   local orientation = get_win_orientation()
@@ -80,7 +51,7 @@ local quick_close_pane = function()
   end
   -- vim.cmd('resize') -- resize help buffer to maximum (CTRL-W__)
   -- vim.cmd('wincmd T') -- move current buffer to new tab
-  nmap('q', ':q<cr>', { buffer = true }) -- map `q` to close help buffer
+  mapper.nmap('q', ':q<cr>', { buffer = true }) -- map `q` to close help buffer
 end
 
 local reload_module = function()
@@ -109,17 +80,17 @@ end
 return {
   is_day = is_day,
   is_gui = is_gui,
-  feedkey = feedkey,
   get_os = get_os,
   get_win_orientation = get_win_orientation,
   quick_close_pane = quick_close_pane,
   reload_module = reload_module,
   autocmd = autocmd,
-  map = map,
-  tmap = tmap,
-  nmap = nmap,
-  vmap = vmap,
-  imap = imap,
-  xmap = xmap,
-  omap = omap,
+  mapper = mapper,
+  map  = mapper.map,
+  tmap = mapper.tmap,
+  nmap = mapper.nmap,
+  vmap = mapper.vmap,
+  imap = mapper.imap,
+  xmap = mapper.xmap,
+  omap = mapper.omap,
 }
--- config/nvim/lua/utils/mapper.lua
+++ config/nvim/lua/utils/mapper.lua
@@ -1,64 +1,48 @@
 -- mapper utils
 
-local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
+---@class mapper
+local M = {}
+
+-- local function replace_termcodes(key, part, lt, special)
+--   return vim.api.nvim_replace_termcodes(key, part, lt, special)
+-- end
+
+-- local function feedkey(key, mode)
+--   return vim.api.nvim_feedkeys(replace_termcodes(key, true, true, true), mode, true)
+-- end
 
 ---@param mode string enum of ""|n|v|i|o|x
 ---@param opts table Mapping options.
-local mapper = function (mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts and type(opts) == 'table' then
-    for key, value in pairs(opts) do
-      options[key] = value
-    end
+local function mapper(mode, lhs, rhs, opts)
+  vim.validate({
+    mode = { mode, { 's', 't' } },
+    lhs = { lhs, 's' },
+    rhs = { rhs, { 's', 'f' } },
+    opts = { opts, 't', true },
+  })
+
+  local default_opts = { noremap = true, silent = true }
+  opts = vim.tbl_extend('force', default_opts, opts or {})
+
+  if type(rhs) == 'function' then
+    opts.callback = rhs
+    rhs = ''
   end
 
-  local bufo = options.buffer
-  local bufnr = (type(bufo) == 'number' and bufo) or (bufo == true and vim.api.nvim_get_current_buf())
-  options.buffer = nil
-
-  if bufnr then
-    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, options)
+  if opts.buffer then
+    local bufnr = opts.buffer == true and 0 or opts.buffer
+    opts.buffer = nil
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
   else
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
   end
 end
 
-local map = function (lhs, rhs, opts)
-  mapper('', lhs, rhs, opts)
+M.setup = function()
+  local modes = { map = '', nmap = 'n', vmap = 'v', imap = 'i', tmap = 't', xmap = 'x', omap = 'o', cmap = 'c' }
+  for map, mode in pairs(modes) do
+    M[map] = function(...) mapper(mode, ...) end
+  end
 end
 
-local tmap = function (lhs, rhs, opts)
-  mapper('t', lhs, rhs, opts)
-end
-
-local nmap = function (lhs, rhs, opts)
-  mapper('n', lhs, rhs, opts)
-end
-
-local vmap = function (lhs, rhs, opts)
-  mapper('v', lhs, rhs, opts)
-end
-
-local imap = function (lhs, rhs, opts)
-  mapper('i', lhs, rhs, opts)
-end
-
-local xmap = function (lhs, rhs, opts)
-  mapper('x', lhs, rhs, opts)
-end
-
-local omap = function (lhs, rhs, opts)
-  mapper('x', lhs, rhs, opts)
-end
-
-return {
-  map = map,
-  tmap = tmap,
-  nmap = nmap,
-  vmap = vmap,
-  imap = imap,
-  xmap = xmap,
-  omap = omap
-}
+return M
