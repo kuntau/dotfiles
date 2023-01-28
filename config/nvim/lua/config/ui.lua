a6929e638f9b1dfca477a87328345844e8bb4e24
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-29 05:21:50 +0800

    feat(nvim): change `colorscheme`
    Add
    - AlexvZyl/nordic.nvim
    - sainnhe/gruvbox-material
    - sainnhe/sonokai
    - sainnhe/edge
    
    Remove
    - kuntau/ayu-vim
    - rakr/vim-one
    - arcticicestudio/nord-vim

--- config/nvim/lua/config/ui.lua
+++ config/nvim/lua/config/ui.lua
@@ -7,8 +7,8 @@ local same_theme = true -- should GUI & TUI have the same theme? GUI use TUI
 local background = (Utils.is_day() and auto_switch) and 'light' or 'dark'
 local gui_light_theme = 'dayfox'
 local gui_dark_theme = 'nightfox'
-local tui_light_theme = 'catppuccin-latte'
-local tui_dark_theme = 'oxocarbon'
+local tui_light_theme = 'edge'
+local tui_dark_theme = 'nordic'
 
 vim.o.background = background
 local tui_theme = background == 'dark' and tui_dark_theme or tui_light_theme
