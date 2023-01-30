35b5bfd0fe0e7b979be7b7c17b46e478b03be383
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-01-31 05:24:58 +0800

    feat(nvim): conceal html class in html & vue
    Less distracting for loooooong class name with tailwind.
    To disable set local `conceallevel` to `0`

---
+++ config/nvim/lua/utils/conceal.lua
@@ -0,0 +1,32 @@
+-- conceal class in html and such. Modified from
+-- https://gist.github.com/mactep/430449fd4f6365474bfa15df5c02d27b
+
+local namespace = vim.api.nvim_create_namespace('class_conceal')
+
+local html_class = function(bufnr)
+  local language_tree = vim.treesitter.get_parser(bufnr, 'html')
+  local syntax_tree = language_tree:parse()
+  local root = syntax_tree[1]:root()
+
+  local query = vim.treesitter.parse_query(
+    'html',
+    [[
+    ((attribute
+        (attribute_name) @att_name (#eq? @att_name "class")
+        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
+    ]]
+  )
+
+  for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_()) do
+    local start_row, start_col, end_row, end_col = captures[2]:range()
+    vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
+      end_line = end_row,
+      end_col = end_col,
+      conceal = metadata[2].conceal,
+    })
+  end
+end
+
+return {
+  html_class = html_class,
+}
