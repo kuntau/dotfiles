ea414f06f527707c03fbe967f9576ebf1a3e7541
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-02-06 16:09:12 +0800

    ref(zsh): simplify existence check

--- zsh/default.zsh
+++ zsh/default.zsh
@@ -1,6 +1,6 @@
 export PAGER='less -S'
-hash bat 2> /dev/null && export PAGER='bat -p' && export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+exists bat && export PAGER='bat -p' && export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 export VISUAL='vim'
 export EDITOR='vim'
-hash nvim 2> /dev/null && export EDITOR='nvim' && export VISUAL='nvim'
+exists nvim && export EDITOR='nvim' && export VISUAL='nvim' && export MANPAGER='nvim +Man!'
