a601b641aa10d555740e8161b20d5356bd3862eb
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-07-19 12:07:20 +0800

    fix(nvim): update `dial` API

--- config/nvim/lua/config/plugins/dial.lua
+++ config/nvim/lua/config/plugins/dial.lua
@@ -5,6 +5,8 @@ return {
       { '<C-x>', '<Plug>(dial-decrement)', mode = {'n','v'}, desc = 'Decrement (Dial)' },
       { 'g<C-a>', 'g<Plug>(dial-increment)', desc = 'Increment (Dial)' },
       { 'g<C-x>', 'g<Plug>(dial-decrement)', desc = 'Decrement (Dial)' },
+      { '<Leader><C-a>', '"=extra<CR><Plug>(dial-increment)', mode = {'n','v'}, desc = 'Increment (Dial+)' },
+      { '<Leader><C-x>', '"=extra<CR><Plug>(dial-decrement)', mode = {'n','v'}, desc = 'Decrement (Dial+)' },
     },
   config = function()
     local augend = require('dial.augend')
@@ -16,13 +18,20 @@ return {
         augend.integer.alias.octal,
         augend.date.alias['%d/%m/%Y'],
         augend.constant.alias.bool,
+      },
+      extra = {
         augend.semver.alias.semver,
         augend.misc.alias.markdown_header,
         augend.paren.alias.quote,
-        -- augend.paren.alias.brackets,
-        -- augend.paren.alias.lua_str_literal,
+        augend.paren.alias.brackets,
+        augend.paren.alias.lua_str_literal,
         augend.constant.new({
           elements = { 'and', 'or' },
+          word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+          cyclic = true, -- "or" is incremented into "and".
+        }),
+        augend.constant.new({
+          elements = { 'yes', 'no' },
           word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
           cyclic = true, -- "or" is incremented into "and".
         }),
