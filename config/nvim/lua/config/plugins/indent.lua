7fca9d61a4e9dfa9c2098e3dc103c54614709d99
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-11-07 01:43:56 +0800

    fix(nvim): update `indentblankline` config for v3

--- config/nvim/lua/config/plugins/indent.lua
+++ config/nvim/lua/config/plugins/indent.lua
@@ -1,43 +1,50 @@
 -- indent_blankline.nvim configs
 
 local config = function()
-  require('indent_blankline').setup({
-    show_current_context = true,
-    space_char_blankline = ' ',
-    show_end_of_line = true,
-    show_first_indent_level = false,
-    indent_blankline_use_treesitter = true,
+  require('ibl').setup({
+    enabled = true,
+    indent = { char = "|" },
+    whitespace = { highlight = { "Whitespace", "NonText" } },
+    scope = {
+      show_start = false,
+      show_end = false,
+    },
+    exclude = {
+      filetypes = {
+        'DiffviewFiles',
+        'FTerm',
+        'Outline',
+        'TelescopePrompt',
+        'TelescopeResults',
+        'alpha',
+        'checkhealth',
+        'git',
+        'gitcommit',
+        'help',
+        'lazy',
+        'lspinfo',
+        'man',
+        'markdown',
+        'nfo',
+        'peek',
+        'startify',
+        'startuptime',
+        '',
+      },
+      buftypes = {
+        'terminal',
+        'nofile',
+        'quickfix',
+        'prompt',
+      }
+    }
   })
-
-  vim.g.indent_blankline_filetype_exclude = {
-    'git',
-    'help',
-    'lspinfo',
-    'markdown',
-    'startify',
-    'startuptime',
-    'checkhealth',
-    'lazy',
-    'peek',
-    'alpha',
-    'Outline',
-    'DiffviewFiles',
-    'FTerm',
-    'nfo',
-    'TelescopePrompt',
-    '',
-  }
-
-  vim.g.indent_blankline_bufname_exclude = {
-    'terminal',
-    'nofile',
-    'quickfix',
-    'prompt',
-  }
 end
 
 return {
   'lukas-reineke/indent-blankline.nvim',
-  cmd = { 'IndentBlanklineEnable', 'IndentBlanklineDisable' },
+  cmd = { 'IBLEnable', 'IBLDisable' },
+  main = 'ibl',
+  opts = {},
   config = config,
 }
