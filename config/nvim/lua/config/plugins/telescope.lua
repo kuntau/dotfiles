eeeaeb36c4020640aa3e5f92a511397be4f15785
Nizamuddin Sulieman <kuntau17@gmail.com>
2023-12-21 15:37:32 -0500

    chore(nvim): update build command

--- config/nvim/lua/config/plugins/telescope.lua
+++ config/nvim/lua/config/plugins/telescope.lua
@@ -114,7 +114,7 @@ return {
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         config = function() require('telescope').load_extension('fzf') end,
-        build = 'make',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
       },
     },
     dev = false,
