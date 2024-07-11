e9389fc15e2a6a78480024f0b9d2d60091e38abe
Nizamuddin Sulieman <kuntau17@gmail.com>
2024-07-11 23:53:53 +0800

    feat(zsh): tweak defaults
    * Add `MARKDOWN_VIEWER` to use better renderer in `glow` or `mdcat` and
      fallback to `bat` or `less` if not available
    * Fix `PAGER` can't set `-p` flag

--- zsh/default.zsh
+++ zsh/default.zsh
@@ -1,6 +1,8 @@
 export PAGER='less -S'
-exists bat && export PAGER='bat -p' && export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+exists bat && export PAGER='bat' && export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 export VISUAL='vim'
 export EDITOR='vim'
 exists nvim && export EDITOR='nvim' && export VISUAL='nvim' && export MANPAGER='nvim +Man!'
+
+export MARKDOWN_VIEWER=$(exists glow && echo 'glow' || echo $PAGER)
