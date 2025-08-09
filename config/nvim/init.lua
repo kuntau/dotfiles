ff77e38b088d8d7614772e336ab4efac679336bc
Nizamuddin Sulieman <kuntau17@gmail.com>
2025-08-10 03:05:33 +0800

    chore(nvim): refined `lazy` configs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,17 +1,19 @@
 -- init.lua --
 
-if vim.fn.has('nvim-0.10') == 1 then
+if vim.loader then
   vim.loader.enable()
 end
 
 require('config.options')
 require('config.lazy')
 require('config.ui')
-require('config.events')
 
+local group = vim.api.nvim_create_augroup("LazyVim", { clear = true })
 vim.api.nvim_create_autocmd("User", {
+  group = group,
   pattern = "VeryLazy",
   callback = function()
+    require('config.events')
     require('config.mappings')
     require('config.commands')
     require('oil').setup()
