2cf97718313e98c4a2672610e9cae8a171246765
Nizamuddin Sulieman <kuntau17@gmail.com>
2025-08-10 03:05:33 +0800

    chore(nvim): refined `lazy` configs

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -2,21 +2,24 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- load lazy
 require("lazy").setup("config.plugins", {
   defaults = { lazy = true },
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "kanagawa", "habamax" } },
   checker = {
     concurrency = 5,
     enabled = true,
@@ -44,9 +47,14 @@ require("lazy").setup("config.plugins", {
   },
   diff = { cmd = 'diffview.nvim' },
   ui = { border = 'rounded' },
+  pkg = { enabled = false },
   rocks = { enabled = false },
   debug = false,
-  dev = {
-    path = "~/coding/forks"
-  }
+  profiling = {
+    loader = false,
+    require = false,
+  },
+  -- dev = {
+  --   path = "~/coding/forks"
+  -- }
 })
