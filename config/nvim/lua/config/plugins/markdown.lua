1649d1bacaa4cce8d0f63c130f85f09db483b537
Nizamuddin Sulieman <kuntau17@gmail.com>
2025-12-09 02:22:00 +0800

    refactor(nvim): more markdown tweaks

--- config/nvim/lua/config/plugins/markdown.lua
+++ config/nvim/lua/config/plugins/markdown.lua
@@ -1,8 +1,16 @@
 -- markdown configs
 
 -- markdown-plus keymaps - https://github.com/yousefhadder/markdown-plus.nvim/wiki/5.Keymaps
+
 obsidian = {
   legacy_commands = false, -- this will be removed in the next major release
+  frontmatter = {
+    enabled = false,
+  },
+  log_level = vim.log.levels.ERROR,
+  daily_notes = {
+    folder = 'Diary',
+  },
   workspaces = {
     {
       name = 'Notes',
@@ -13,8 +21,8 @@ obsidian = {
 
 return {
   { 'yousefhadder/markdown-plus.nvim', config = true, ft = 'markdown' },
-  { 'MeanderingProgrammer/markdown.nvim', main = 'render-markdown', config = true, ft = 'markdown' },
   { 'toppair/peek.nvim', config = true, build = 'deno task --quiet build:fast', ft = 'markdown' }, -- Markdown preview
-  { 'obsidian-nvim/obsidian.nvim', version = '*', opts = obsidian, ft = 'markdown' },
-  -- { 'OXY2DEV/markview.nvim', ft = 'markdown' }, -- TODO: Torned between this or `markdown.nvim`
+  { 'obsidian-nvim/obsidian.nvim', enabled = true, version = '*', opts = obsidian, ft = 'markdown' },
+  { 'OXY2DEV/markview.nvim', ft = 'markdown' }, -- TODO: Torned between this or `markdown.nvim`
+  -- { 'MeanderingProgrammer/render-markdown.nvim', config = true, ft = 'markdown' },
 }
