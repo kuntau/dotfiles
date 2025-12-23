78762611bd97bd6d07697f33dae4c2f89147086d
Kuntau <kuntau17@gmail.com>
2025-12-24 03:41:32 +0800

    chore(yazi): refine configs

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -334,3 +334,11 @@ fi
 }
 zle -N fg-bg
 bindkey '^z' fg-bg # CTRL+z
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
