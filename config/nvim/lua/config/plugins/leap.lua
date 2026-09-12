100e88ec98b0bd4cf6e0db6d1517866ad958085d
Kuntau <kuntau17@gmail.com>
2026-09-13 01:54:45 +0800

    chore(nvim): update leap configs. remote -> visit
    reference: https://codeberg.org/andyg/leap.nvim/commit/e35ded91a023f432d03473eaefc584d8156cf80f

--- config/nvim/lua/config/plugins/leap.lua
+++ config/nvim/lua/config/plugins/leap.lua
@@ -6,8 +6,6 @@ return {
     event = 'VeryLazy',
     config = function()
       local leap = require('leap')
-      vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-      vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
       -- leap.opts.case_sensitive = true
       -- leap.opts.max_phase_one_targets = 5
       -- leap.opts.highlight_unlabeled_phase_one_targets = true
@@ -15,31 +13,38 @@ return {
       -- Use the traversal keys to repeat the previous motion without explicitly invoking Leap:
       require('leap.user').set_repeat_keys('<tab>', '<backspace>')
 
-      -- Treesitter node selection (vRRR...y or yR{label}):
-      vim.keymap.set({'x', 'o'}, 'R',  function ()
-        require('leap.treesitter').select {
-          opts = require('leap.user').with_traversal_keys('R', 'r')
-        }
-      end)
+      vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+      vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
 
-      -- Remote operations (gs{leap}apy or ygs{leap}ap, where {leap} means {char1}{char2}{label?}, as usual):
-      vim.keymap.set({'n', 'o'}, 'gs', function ()
-        require('leap.remote').action {
-          -- Automatically enter Visual mode when coming from Normal.
-          input = vim.fn.mode(true):match('o') and '' or 'v'
-        }
-      end)
+      -- Visit (jump - operate - jump back)
+      vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-visit)')
+      vim.keymap.set({ 'x', 'o' },      'ar', '<Plug>(leap-visit-text-object)')
+      vim.keymap.set({ 'x', 'o' },      'ir', '<Plug>(leap-visit-inner-text-object)')
+
+      vim.keymap.set('o', 'rr', function()  -- "visit line" shortcut
+        return (vim.v.count == 0 and '1' or '') .. '<Plug>(leap-visit)'
+      end, { expr = true })
 
       -- Automatic paste after remote yank operations:
       vim.api.nvim_create_autocmd('User', {
-        pattern = 'RemoteOperationDone',
-        group = vim.api.nvim_create_augroup('LeapRemote', {}),
-        callback = function (event)
-          if vim.v.operator == 'y' and event.data.register == '"' then
-            vim.cmd('normal! p')
-          end
-        end,
-      })
+        pattern = 'VisitDone',
+        group = vim.api.nvim_create_augroup('Visit', {}),
+        callback = function(event)
+          if
+            (event.data.mode:match('^[vV\22]') or (vim.v.operator == 'y'))
+            and event.data.register == '"'
+            then
+              vim.cmd('normal! p')
+            end
+          end,
+        })
+
+      -- Treeselect
+      vim.keymap.set({ 'x', 'o' }, 'an', function()
+        require('leap.treesitter').select {
+          opts = require('leap.user').with_traversal_keys('n', 'N')
+        }
+      end)
 
       vim.keymap.set({'n', 'x', 'o'}, '|', function ()
         local line = vim.fn.line('.')
